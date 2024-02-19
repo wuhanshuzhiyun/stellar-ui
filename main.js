6548079fe7c3a9f4d7b6ce049f8ba403bd17f289
徐亚军 <850674935@qq.com>
2024-02-19 08:36:31 +0800

    feat(save): 初始化组件结构

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import App from './App';
 // #ifndef VUE3
 import Vue from 'vue';
 import './uni.promisify.adaptor';
+// #ifdef H5
 import { router, RouterMount } from './router/index.js';
 Vue.use(router);
+// #endif
 Vue.config.productionTip = false;
 App.mpType = 'app';
 const app = new Vue({
---
+++ uni_modules/stellar-ui/components/ste-test/ste-test.vue
@@ -0,0 +1,7 @@
+<template>
+    <view class="content">测试组件</view>
+</template>
+
+<script></script>
+
+<style></style>
