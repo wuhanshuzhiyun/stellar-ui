6d4ace23a7e7f15dcbaedbbb110ff33b3b3b7c48
徐亚军 <850674935@qq.com>
2024-02-19 14:51:46 +0800

    feat(save): 调整样式

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,9 @@
-const TransformPages = require('uni-read-pages');
-const { webpack } = new TransformPages();
 // vue.config.js
 module.exports = {
-    // ...,
+	// ...,
 
-    chainWebpack: (config) => {
-        config.resolveLoader.alias.set('file-loader', __dirname + '/node_modules/file-loader');
-        config.module.rule('markdown').test(/\.md$/).use('file-loader').loader('file-loader').end();
-    },
-    configureWebpack: {
-        plugins: [
-            new webpack.DefinePlugin({
-                ROUTES: webpack.DefinePlugin.runtimeValue(() => {
-                    const tfPages = new TransformPages({
-                        includes: ['path', 'name', 'aliasPath']
-                    });
-                    return JSON.stringify(tfPages.routes);
-                }, true)
-            })
-        ]
-    }
+	chainWebpack: (config) => {
+		config.resolveLoader.alias.set('file-loader', __dirname + '/node_modules/file-loader');
+		config.module.rule('markdown').test(/\.md$/).use('file-loader').loader('file-loader').end();
+	},
 };
