fa0855da6be4b70eb429669b3d3d20ee251191de
张园因 <30020100@qq.com>
2024-02-23 11:41:58 +0800

    更新icon组件预览页

--- mp/icon-demo/icon-demo.vue
+++ mp/icon-demo/icon-demo.vue
@@ -1,12 +1,13 @@
 <template>
 	<view class="page">
 		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="图标"></page-nav>
-		<view class="wrapper">
-			<view class="demo-block">
-				<view class="title">基础用法</view>
-				<view class="block-content">
-					<ste-search @search="onSearch" />
+		<view class="icon-box">
+			<view v-for="item in glyphs" class="icon-item">
+				<view class="icon-content">
+					<ste-icon :code="item.unicode" :size="40"></ste-icon>
 				</view>
+				<view class="icon-name">{{ item.name }}</view>
+				<view class="icon-unicode">{{ item.unicode }}</view>
 			</view>
 		</view>
 	</view>
@@ -15,47 +16,57 @@
 export default {
 	data() {
 		return {
-			keyword: 'RTX4090 Ti',
+			iconUrl:
+				'https://at.alicdn.com/t/c/font_4041637_pivqtx3f1mq.json?spm=a313x.manage_type_myprojects.i1.49.f7ba3a81fFvJ6W&file=font_4041637_pivqtx3f1mq.json',
+			glyphs: [],
 		};
 	},
-	onLoad() {},
-	methods: {
-		onSearch(v) {
-			console.log('search', v);
-			uni.showToast({
-				icon: 'none',
-				title: `搜索：${v}`,
-			});
-		},
+	onReady() {},
+	onLoad() {
+		uni.request({
+			url: this.iconUrl,
+			success: (res) => {
+				this.glyphs = res.data.glyphs;
+				this.glyphs.forEach((item) => {
+					item.unicode = '&#x' + item.unicode + ';';
+				});
+			},
+		});
 	},
+	methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
 .page {
-	.wrapper {
-		padding: 16rpx;
+	padding-bottom: 40rpx;
+}
+.icon-box {
+	display: flex;
+	flex-wrap: wrap;
+	row-gap: 60rpx;
+	justify-content: center;
+	align-items: center;
+}
+.icon-item {
+	width: 25%;
+	.icon-content {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		padding-bottom: 20rpx;
+	}
 
-		.demo-block {
-			margin-bottom: 16rpx;
-
-			.title {
-				font-size: 30rpx;
-				color: #8f9ca2;
-				margin-bottom: 8rpx;
-			}
-
-			.block-content {
-				display: flex;
-				flex-direction: row;
-				flex-wrap: wrap;
-				align-items: center;
-
-				> * {
-					margin: 0 16rpx 16rpx 0;
-				}
-			}
-		}
+	.icon-name {
+		text-align: center;
+		overflow: hidden;
+		height: 40rpx;
+		line-height: 40rpx;
+		font-size: 28rpx;
+	}
+	.icon-unicode {
+		text-align: center;
 	}
 }
 </style>
