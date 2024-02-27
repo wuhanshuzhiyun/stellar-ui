8c1eacffda20b797b326e522e8df10ae254f93d5
付宇威 <925720486@qq.com>
2024-02-27 16:21:08 +0800

    修改规则命名

--- mp/search-demo/search-demo.vue
+++ mp/search-demo/search-demo.vue
@@ -1,35 +1,35 @@
 <template>
 	<view class="page">
 		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="搜索"></page-nav>
-		<view class="wrapper">
-			<view class="demo-block">
+		<view class="content">
+			<view class="demo-item">
 				<view class="title">基础功能</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search v-model="value1" :show-action="false" @change="change"></ste-search>
 				</view>
 			</view>
-			<view class="demo-block">
+			<view class="demo-item">
 				<view class="title">设置初始值</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search v-model="value2" :show-action="false"></ste-search>
 				</view>
 			</view>
-			<view class="demo-block">
+			<view class="demo-item">
 				<view class="title">搜索框形状</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search v-model="value3" :show-action="false" shape="round"></ste-search>
 					<ste-search v-model="value4" :show-action="false" shape="square" :margin="margin"></ste-search>
 				</view>
 			</view>
-			<view class="demo-block">
+			<view class="demo-item">
 				<view class="title">右侧控件</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search v-model="value5" @custom="customClick"></ste-search>
 				</view>
 			</view>
-			<view class="demo-block">
+			<view class="demo-item">
 				<view class="title">禁用输入框</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search
 						placeholder="输入框被禁用,可以监听点击事件进行跳转"
 						disabled
@@ -38,15 +38,15 @@
 					></ste-search>
 				</view>
 			</view>
-			<!-- 			<view class="demo-block">
+			<!-- 			<view class="demo-item">
 				<view class="title">点击左侧图标</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search v-model="value6" :show-action="false" @clickIcon="clickIcon"></ste-search>
 				</view>
 			</view> -->
-			<!-- <view class="demo-block">
+			<!-- <view class="demo-item">
 				<view class="title">搜索框内容水平对齐</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search v-model="value7" :show-action="false" input-align="left"></ste-search>
 					<ste-search
 						v-model="value8"
@@ -57,9 +57,9 @@
 					<ste-search v-model="value9" :show-action="false" input-align="right" :margin="margin"></ste-search>
 				</view>
 			</view> -->
-			<view class="demo-block">
+			<view class="demo-item">
 				<view class="title">自定义</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search
 						v-model="value10"
 						:show-action="false"
@@ -97,15 +97,15 @@
 				</view>
 			</view>
 
-			<!-- view class="demo-block">
+			<!-- view class="demo-item">
 				<view class="title">热词列表</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search :hot-words="hotWords" @search="onSearch" />
 				</view>
 			</view>
-			<view class="demo-block">
+			<view class="demo-item">
 				<view class="title">导航</view>
-				<view class="block-content">
+				<view class="item-block">
 					<ste-search :style-type="2" @nav-address="onNavAddress" />
 				</view>
 			</view> -->
@@ -189,8 +189,8 @@ export default {
 <style lang="scss" scoped>
 .page {
 	.wrapper {
-		.demo-block {
-			.block-content {
+		.demo-item {
+			.item-block {
 				display: block;
 				> .ste-search {
 					margin: 0 8px 8px 0;
