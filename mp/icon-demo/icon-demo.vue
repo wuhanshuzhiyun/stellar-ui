688cc3adcbda8bfadb4da144c61b50292ed76dcd
张园因 <30020100@qq.com>
2024-02-29 15:00:44 +0800

    更新icon预览页的样式， md文档

--- mp/icon-demo/icon-demo.vue
+++ mp/icon-demo/icon-demo.vue
@@ -2,89 +2,43 @@
 	<view class="page">
 		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="图标"></page-nav>
 		<view class="content">
-			<view class="demo-item">
-				<view class="title">基础用法</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" size="60"></ste-icon>
-				</view>
+			<view class="tabs">
+				<view class="tab1-title" :class="{ actived: tabIndex === 0 }" @click="tabIndex = 0">用法示例</view>
+				<view class="tab2-title" :class="{ actived: tabIndex === 1 }" @click="tabIndex = 1">图标库</view>
 			</view>
-			<view class="demo-item">
-				<view class="title">图标颜色</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" color="#1989fa" size="60" marginRight="30"></ste-icon>
-					<ste-icon code="&#xe6fa;" color="#ee0a24" size="60"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">图标大小</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" marginRight="30"></ste-icon>
-					<ste-icon code="&#xe6fa;" size="100"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">图标粗细</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" size="60" marginRight="30"></ste-icon>
-					<ste-icon code="&#xe6fa;" size="60" weight="bold"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">图标边距</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<view>
-						<ste-icon code="&#xe6fa;" size="60" marginTop="30"></ste-icon>
-						上边距
+			<view v-if="tabIndex === 0" class="tab1-content">
+				<view class="demo-item">
+					<view class="title">基础用法</view>
+					<view class="item-block" style="justify-content: flex-start">
+						<ste-icon code="&#xe653;" size="60"></ste-icon>
 					</view>
-					<view>
-						<ste-icon code="&#xe6fa;" size="60" marginRight="30"></ste-icon>
-						右边距
+				</view>
+				<view class="demo-item">
+					<view class="title">图标颜色</view>
+					<view class="item-block" style="justify-content: flex-start">
+						<ste-icon code="&#xe64b;" color="#1989fa" size="60" marginRight="30"></ste-icon>
+						<ste-icon code="&#xe64b;" color="#ee0a24" size="60"></ste-icon>
 					</view>
-					<view>
-						<ste-icon code="&#xe6fa;" size="60" marginBottom="30"></ste-icon>
-						下边距
-					</view>
-					<view>
-						<ste-icon code="&#xe6fa;" size="60" marginLeft="30"></ste-icon>
-						左边距
+				</view>
+				<view class="demo-item">
+					<view class="title">图标大小</view>
+					<view class="item-block" style="justify-content: flex-start">
+						<ste-icon code="&#xe628;" marginRight="30"></ste-icon>
+						<ste-icon code="&#xe628;" marginRight="30" size="50"></ste-icon>
+						<ste-icon code="&#xe628;" marginRight="30" size="70"></ste-icon>
 					</view>
 				</view>
 			</view>
-			<view class="demo-item">
-				<view class="title">内嵌文字</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" size="60" text="图标" textColor="#000"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">内嵌文字颜色</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" size="60" text="图标" textColor="red"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">内嵌字体大小</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" size="60" text="图标" textColor="#000" marginRight="30"></ste-icon>
-					<ste-icon code="&#xe6fa;" size="60" text="图标" textColor="#000" textSize="30"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">内嵌字体粗细</view>
-				<view class="item-block" style="justify-content: flex-start">
-					<ste-icon code="&#xe6fa;" size="60" text="图标" textColor="#000" marginRight="30"></ste-icon>
-					<ste-icon code="&#xe6fa;" size="60" text="图标" textColor="#000" textWeight="bold"></ste-icon>
-				</view>
-			</view>
-			<view class="demo-item">
-				<view class="title">图标库</view>
-				<view class="item-block">
-					<view v-for="item in glyphs" class="icon-item" @click="copy(item.unicode)">
-						<view class="icon-content">
-							<ste-icon :code="item.unicode" :size="40"></ste-icon>
+			<view v-if="tabIndex === 1" class="tab2-content">
+				<view class="demo-item">
+					<view class="item-block">
+						<view v-for="item in glyphs" class="icon-item" @click="copy(item.unicode)">
+							<view class="icon-content">
+								<ste-icon :code="item.unicode" :size="40"></ste-icon>
+							</view>
+							<view class="icon-name">{{ item.name }}</view>
+							<view class="icon-unicode">{{ item.unicode }}</view>
 						</view>
-						<view class="icon-name">{{ item.name }}</view>
-						<view class="icon-unicode">{{ item.unicode }}</view>
 					</view>
 				</view>
 			</view>
@@ -98,6 +52,7 @@ export default {
 			iconUrl:
 				'https://at.alicdn.com/t/c/font_4041637_pivqtx3f1mq.json?spm=a313x.manage_type_myprojects.i1.49.f7ba3a81fFvJ6W&file=font_4041637_pivqtx3f1mq.json',
 			glyphs: [],
+			tabIndex: 0,
 		};
 	},
 	onReady() {},
@@ -139,6 +94,7 @@ export default {
 
 	.icon-item {
 		width: 25%;
+
 		.icon-content {
 			display: flex;
 			flex-wrap: wrap;
@@ -154,10 +110,30 @@ export default {
 			line-height: 40rpx;
 			font-size: 28rpx;
 		}
+
 		.icon-unicode {
 			font-size: 28rpx;
 			text-align: center;
 		}
+	}
+}
+
+.tabs {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: nowrap;
+	height: 70rpx;
+	border-bottom: 1px solid #eee;
+	margin-bottom: 20rpx;
+	.tab1-title,
+	.tab2-title {
+		width: 50%;
+		text-align: center;
+		font-size: 32rpx;
+	}
+	.actived {
+		font-weight: bold;
 	}
 }
 </style>
