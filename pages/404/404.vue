71bb2c2a129e130095be5713b58cc91d4588d4d7
秦鹏飞 <q1506589520q@gmail.com>
2024-03-05 15:35:35 +0800

    组件库添加路由功能和配置，添加404功能

---
+++ pages/404/404.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="page">
+		<image :src="imageUrl"></image>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			imageUrl: require('@/static/404.png'),
+		};
+	},
+};
+</script>
+<style lang="scss" scoped>
+.page {
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	image {
+		width: 50vw;
+		height: 50vh;
+	}
+}
+</style>
