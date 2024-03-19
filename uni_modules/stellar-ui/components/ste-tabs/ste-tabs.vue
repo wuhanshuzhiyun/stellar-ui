8acecf33e464e40df7033ae07ca650bfb015b111
徐亚军 <850674935@qq.com>
2024-03-19 15:07:30 +0800

    feat(save): save

--- uni_modules/stellar-ui/components/ste-tabs/ste-tabs.vue
+++ uni_modules/stellar-ui/components/ste-tabs/ste-tabs.vue
@@ -93,14 +93,6 @@
 <script>
 import utils from '../../utils/utils.js';
 import { getChildrenProps } from './utils.js';
-import props from './props.js';
-
-let _tabsComponent = {
-	getParent() {
-		return this;
-	},
-};
-let _timeout;
 /**
  * ste-tabs 标签页
  * @description 标签页组件
@@ -112,8 +104,144 @@ export default {
 	group: '基础组件',
 	title: 'Tabs 标签页',
 	name: 'ste-tabs',
-	mixins: [props],
-	provide: { _tabsComponent },
+	props: {
+		// 当前激活的选项（支持v-model双向绑定）（类型为number时绑定index，类型为string时绑定name）
+		active: {
+			type: [Number, String],
+			default: () => null,
+		},
+		// 样式风格类型，可选值为 line  card
+		type: {
+			type: String,
+			default: () => 'line',
+		},
+		// 是否显示图片
+		showImage: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否显示主标题
+		showTitle: {
+			type: Boolean,
+			default: () => true,
+		},
+		// 是否显示副标题
+		showSubtitle: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 主题色（滑块颜色，边框颜色，选中的背景色，激活下拉列表中选项颜色）
+		color: {
+			type: String,
+			default: () => '#0090FF',
+		},
+		// 背景
+		background: {
+			type: String,
+			default: () => 'none',
+		},
+		// 动画时间，单位秒，设置为 0 可以禁用动画
+		duration: {
+			type: [String, Number],
+			default: () => 0.3,
+		},
+		// 底部条宽度，默认单位rpx
+		lineWidth: {
+			type: [String, Number],
+			default: () => 52,
+		},
+		// 底部条高度，默认单位rpx
+		lineHeight: {
+			type: [String, Number],
+			default: () => 6,
+		},
+		// 是否显示标签栏外边框，仅在 type="line" 时有效
+		border: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否省略过长的标题文字
+		ellipsis: {
+			type: Boolean,
+			default: () => false,
+		},
+		// tab内容的宽度
+		tabWidth: {
+			type: [String, Number],
+			default: () => 'auto',
+		},
+		// tab等分数量，设置为 0 ，则不等分
+		divideNum: {
+			type: Number,
+			default: () => 4,
+		},
+		// tab间距rpx
+		tabSpace: {
+			type: Number,
+			default: () => 0,
+		},
+		// 是否使用粘性布局
+		sticky: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否开启手势左右滑动切换
+		swipeable: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否开启滚动导航
+		scrollspy: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 粘性布局下吸顶时与顶部的距离，单位rpx
+		offsetTop: {
+			type: [String, Number],
+			default: () => 0,
+		},
+		// 选项字体颜色和下拉列表中选项颜色
+		tabColor: {
+			type: String,
+			default: () => '#000000',
+		},
+		// 激活选项字体颜色
+		activeTabColor: {
+			type: String,
+			default: () => '#000000',
+		},
+		// 是否显示分隔符
+		showGapLine: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否锁定tab(无法切换)
+		lock: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否禁用tab(所有功能失效)
+		disabled: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 是否有下拉选择按钮
+		pullDown: {
+			type: Boolean,
+			default: () => false,
+		},
+		// 下拉占位符
+		placeholder: {
+			type: String,
+			default: () => '选择类别',
+		},
+	},
+
+	provide() {
+		return {
+			_tabsComponent: { getParent: () => this },
+		};
+	},
 	data() {
 		return {
 			viewScrollLeft: 0,
@@ -125,10 +253,8 @@ export default {
 			tabEls: [],
 			openPullDown: false,
 			pullListTransform: '-100%',
+			_timeout: null,
 		};
-	},
-	beforeCreate() {
-		_tabsComponent.getParent = _tabsComponent.getParent.bind(this);
 	},
 	computed: {
 		cmpTabList() {
@@ -256,13 +382,6 @@ export default {
 		},
 	},
 	watch: {
-		$props: {
-			handler(v) {
-				_tabsComponent.props = v;
-			},
-			immediate: true,
-			deep: true,
-		},
 		cmpActiveIndex: {
 			handler(v) {
 				this.scrollToActive();
@@ -273,11 +392,11 @@ export default {
 	methods: {
 		updateTabs() {
 			this.showComponent = false;
-			clearTimeout(_timeout);
-			_timeout = setTimeout(() => {
-				console.log('----updateTabs----');
+			clearTimeout(this._timeout);
+			this._timeout = setTimeout(() => {
 				this.tabPropsList = getChildrenProps(this, 'ste-tab');
 				this.showComponent = true;
+				console.log('----updateTabs----');
 				this.$nextTick(async () => {
 					this.listBoxEl = await utils.querySelector('.tab-list-box', this);
 					this.listEl = await utils.querySelector('.tab-list-box .tab-list.view-list', this);
