44d672219098126dece0652ab9dbd79a400553e6
付宇威 <925720486@qq.com>
2024-03-19 17:20:49 +0800

    进度条组件

--- mp/index/index.vue
+++ mp/index/index.vue
@@ -45,6 +45,13 @@
 			</view>
 			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 		</view>
+		<view class="com-box" @click="nav('progress')">
+			<view class="com-title">
+				<image :src="joinSrc('img/progress.png')" style="width: 36rpx; height: 36rpx" />
+				<text>Progress 进度条</text>
+			</view>
+			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
+		</view>
 		<!-- ***电商组件*** -->
 		<view class="group-title">电商组件</view>
 		<view class="com-box" @click="nav('price')">
