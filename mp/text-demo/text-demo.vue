fd48092078234b729539c66ca62ae0f2e12f329d
付宇威 <925720486@qq.com>
2024-03-20 16:11:55 +0800

    文本组件初始化

--- mp/text-demo/text-demo.vue
+++ mp/text-demo/text-demo.vue
@@ -1,22 +1,11 @@
 <template>
 	<view class="page">
-		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="按钮"></page-nav>
+		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="文本"></page-nav>
 		<view class="content">
 			<view class="demo-item">
-				<view class="title">按钮大小</view>
+				<view class="title">文本</view>
 				<view class="item-block">
-					<view>
-						<ste-button :mode="400" @click="handleClick('超大按钮')">超大按钮</ste-button>
-					</view>
-					<view>
-						<ste-button :mode="300" @click="handleClick('大按钮')">大按钮</ste-button>
-					</view>
-					<view>
-						<ste-button :mode="200" @click="handleClick('中按钮')">中按钮</ste-button>
-					</view>
-					<view>
-						<ste-button :mode="100" @click="handleClick('小按钮')">小按钮</ste-button>
-					</view>
+					<view></view>
 				</view>
 			</view>
 		</view>
