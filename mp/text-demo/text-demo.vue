2cdcf734c956791fd54868ec91103d9873a1a2b7
付宇威 <925720486@qq.com>
2024-03-20 16:10:34 +0800

    文本组件

---
+++ mp/text-demo/text-demo.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="page">
+		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="按钮"></page-nav>
+		<view class="content">
+			<view class="demo-item">
+				<view class="title">按钮大小</view>
+				<view class="item-block">
+					<view>
+						<ste-button :mode="400" @click="handleClick('超大按钮')">超大按钮</ste-button>
+					</view>
+					<view>
+						<ste-button :mode="300" @click="handleClick('大按钮')">大按钮</ste-button>
+					</view>
+					<view>
+						<ste-button :mode="200" @click="handleClick('中按钮')">中按钮</ste-button>
+					</view>
+					<view>
+						<ste-button :mode="100" @click="handleClick('小按钮')">小按钮</ste-button>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+export default {
+	data() {
+		return {};
+	},
+	created() {},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+	.content {
+		.demo-item {
+			.item-block {
+				> view {
+					margin: 0 8px 8px 0;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ uni_modules/stellar-ui/components/ste-text/ste-text.vue
@@ -0,0 +1,30 @@
+<template>
+	<text class="ste-text--root"></text>
+</template>
+
+<script>
+import utils from '../../utils/utils.js';
+/**
+ * ste-text 文本
+ * @description 文本组件
+ * @tutorial https://stellar-ui.intecloud.com.cn/pc/index/index?name=ste-text
+ */
+
+export default {
+	group: '基础组件',
+	title: 'Text 文本',
+	name: 'ste-text',
+	props: {},
+	data() {
+		return {};
+	},
+	created() {},
+	computed: {},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.ste-text--root {
+}
+</style>
