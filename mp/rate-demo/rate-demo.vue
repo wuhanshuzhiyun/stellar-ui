61322a16b0c60b428b93e70c0b6d57e6da0b24e3
秦鹏飞 <q1506589520q@gmail.com>
2024-03-21 17:12:47 +0800

    评分组件和文本完成

--- mp/rate-demo/rate-demo.vue
+++ mp/rate-demo/rate-demo.vue
@@ -1,9 +1,93 @@
 <template>
-	<div style="margin-top: 400rpx">
-		<ste-rate></ste-rate>
-	</div>
+	<view class="page">
+		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="评分"></page-nav>
+		<view class="content">
+			<view class="demo-item">
+				<view class="title">基础用法</view>
+				<view class="item-block">
+					<ste-rate v-model="value"></ste-rate>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">自定义图标</view>
+				<view class="item-block">
+					<div class="item-rate">
+						<!--选中和未选中 图标一样-->
+						<ste-rate v-model="value1" inactiveCode="&#xe685;" activeCode="&#xe685;"></ste-rate>
+						<text>选中和未选中 图标一样的情况</text>
+						<!--选中和未选中 图标不一样-->
+						<ste-rate v-model="value1" inactiveCode="&#xe685;" activeCode="&#xe687;"></ste-rate>
+						<text>选中和未选中 图标不一样的情况</text>
+					</div>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">自定义样式</view>
+				<view class="item-block">
+					<ste-rate
+						v-model="value2"
+						inactiveColor="#F8D5D5"
+						activeColor="#d276a3"
+						size="80"
+						:gutter="20"
+					></ste-rate>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">只读</view>
+				<view class="item-block">
+					<ste-rate v-model="value3" readonly></ste-rate>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">禁用</view>
+				<view class="item-block">
+					<ste-rate v-model="value3" disabled></ste-rate>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">自定义数量和自定义分值</view>
+				<view class="item-block">
+					<div class="item-rate">
+						<!--星星数量为1，分值为5, 分数为2.5-->
+						<ste-rate v-model="value4" :count="1" :score="5"></ste-rate>
+						<text>图标数量为1，图标分值为5, 分数为2.5</text>
+					</div>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
-<script></script>
+<script>
+export default {
+	data() {
+		return {
+			value: 2,
+			value1: 2,
+			value2: 3,
+			value3: 2.6,
+			value4: 2.5,
+		};
+	},
+	created() {},
+	methods: {
+		change(value) {
+			console.log('value', value);
+		},
+	},
+};
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page {
+	.item-rate {
+		display: flex;
+		flex-direction: column;
+
+		text {
+			font-size: 30rpx;
+		}
+	}
+}
+</style>
--- uni_modules/stellar-ui/components/ste-rate/ste-rate.vue
+++ uni_modules/stellar-ui/components/ste-rate/ste-rate.vue
@@ -1,12 +1,12 @@
 <template>
 	<view class="ste-rate--root">
 		<view class="list" :style="[cmpListStyle]">
-			<view v-for="index in count" class="item">
-				<view class="inactive" @click="onSelect(index)">
+			<view v-for="index in cmpCount" class="item">
+				<view class="icon-box inactive" @click="onSelect(index)">
 					<ste-icon :code="inactiveCode" :color="inactiveColor" :size="size"></ste-icon>
 				</view>
-				<view class="active" @click="onSelect(index)">
-					<ste-icon :code="activeCode" :color="activeColor" :size="size"></ste-icon>
+				<view class="icon-box active" @click="onSelect(index)" :style="{ width: getWidth(index) }">
+					<ste-icon :code="activeCode" :color="disabled ? '#C8C9CC' : activeColor" :size="size"></ste-icon>
 				</view>
 			</view>
 		</view>
@@ -17,19 +17,20 @@
 import utils from '../../utils/utils.js';
 /**
  * ste-rate 评分
- * @description 评分组件
+ * @description 评分组件,用于对事物进行评级操作。
  * @tutorial https://stellar-ui.intecloud.com.cn/pc/index/index?name=ste-rate
  * @property {Number} value 当前评分数（支持v-model双向绑定）
  * @property {Number} count 图标总数 默认 5
  * @property {Number} score 每颗星星代表的分数 默认 1
  * @property {Boolean} disabled 禁用 默认 false
  * @property {Boolean} readonly 只读（不置灰） 默认 false
- * @property {Number|String} size 评分图标的大小，单位rpx 默认 50
+ * @property {Number|String} size 评分图标的大小，单位rpx 默认 44
  * @property {String} inactiveColor 未选中的颜色 默认 #dddddd
  * @property {String} activeColor 选中的颜色 默认 #fa5014
  * @property {String} inactiveCode 未选中的图标code 默认 &#xe681;
  * @property {String} activeCode 选中的图标code 默认 &#xe684;
- * @property {Number|String} gutter 每个图标之间的距离，单位rpx 默认 5
+ * @property {Number|String} gutter 每个图标之间的距离，单位rpx 默认 10
+ * @event {Function} change 当前分值变化时触发的事件
  */
 
 export default {
@@ -59,7 +60,7 @@ export default {
 		},
 		size: {
 			ttype: [String, Number],
-			default: 50,
+			default: 44,
 		},
 		inactiveColor: {
 			ttype: String,
@@ -79,8 +80,13 @@ export default {
 		},
 		gutter: {
 			ttype: [String, Number],
-			default: 5,
+			default: 10,
 		},
+	},
+	model: {
+		prop: 'value',
+		// 派发事件名，更新父组件数组
+		event: 'input',
 	},
 	data() {
 		return {};
@@ -91,21 +97,32 @@ export default {
 			style['columnGap'] = this.gutter + 'rpx';
 			return style;
 		},
+		cmpCount() {
+			// 兼容浏览器和微信 对数字循环的处理不同
+			return Array.from({ length: this.count }, (_, index) => index);
+		},
 	},
 	methods: {
 		onSelect(index) {
-			console.log('index', index);
-			return;
-			var score = event.currentTarget.dataset.score;
 			if (!this.disabled && !this.readonly) {
-				this.this.setData({ innerValue: score + 1 });
-				if ((0, version_1.canIUseModel)()) {
-					this.setData({ value: score + 1 });
+				let value = (index + 1) * this.score;
+				if (this.value != value) {
+					// 更新 model
+					this.$emit('input', value);
+					// change回调
+					this.$emit('change', value);
 				}
-				wx.nextTick(function () {
-					_this.$emit('input', score + 1);
-					_this.$emit('change', score + 1);
-				});
+			}
+		},
+		getWidth(index) {
+			let value = (index + 1) * this.score;
+			let diff = this.value - value;
+			if (diff >= 0) {
+				return '100%';
+			} else if (Math.abs(diff) > 0 && Math.abs(diff) < this.score) {
+				return (1 - Math.abs(diff) / this.score).toFixed(2) * 100 + '%';
+			} else {
+				return 0;
 			}
 		},
 	},
@@ -116,6 +133,16 @@ export default {
 .ste-rate--root {
 	.list {
 		display: flex;
+
+		.item {
+			position: relative;
+
+			.active {
+				position: absolute;
+				overflow: hidden;
+				top: 0;
+			}
+		}
 	}
 }
 </style>
