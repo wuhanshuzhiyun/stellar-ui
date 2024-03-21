006dc6fa158e9a5f9b53b723748c042def994862
徐亚军 <850674935@qq.com>
2024-03-21 16:54:12 +0800

    feat(save): save

--- uni_modules/stellar-ui/components/ste-tabs/ste-tabs.vue
+++ uni_modules/stellar-ui/components/ste-tabs/ste-tabs.vue
@@ -96,6 +96,7 @@
 <script>
 import utils from '../../utils/utils.js';
 import { getChildrenProps } from './utils.js';
+import props from './props.js';
 /**
  * ste-tabs 标签页
  * @description 标签页组件
@@ -110,209 +111,43 @@ import { getChildrenProps } from './utils.js';
  * @property {String}						color							主题色（滑块颜色，边框颜色，选中的背景色，激活下拉列表中选项颜色），默认#0090FF
  * @property {String} 					background				背景
  * @property {Number} 					duration					切换动画时长，默认0.3，单位秒
- * @property {String | Number}	lineWidth					底部条(或副标题)宽度，默认52，单位rpx
- * @property {String | Number}	lineHeight				底部条(或副标题)高度，默认6，单位rpx
+ * @property {Boolean}					showLine					底部条是否显示，默认true（显示副标题的情况下会自动隐藏）
+ * @property {String | Number}	lineWidth					底部条宽度，默认52，单位rpx
+ * @property {String | Number}	lineHeight				底部条高度，默认6，单位rpx
  * @property {Boolean}					border						是否显示边框，仅在card模式下生效
  * @property {Boolean}					ellipsis					是否开启文本超长省略
  * @property {String | Number}	tabWidth					选项宽度，默认auto，单位rpx
- * @property {Number}						divideNum					均分最大数量，默认4，设置0则不均分
+ * @property {String | Number}	tabPadding				选项内边距，默认24，单位rpx
  * @property {String | Number}	tabSpace					选项间距，默认0，单位rpx
+ * @property {Number}						divideNum					均分最大数量，默认4，设置0则不均分
  * @property {Boolean}					sticky						是否开启吸顶
  * @property {Number | String}	offsetTop					吸顶距离
  * @property {Boolean} 					swipeable					是否开启手势滑动切换
- * @property {String} 					titleColor				主标题字体颜色和下拉列表中选项颜色，默认值#000000
- * @property {String} 					activeTitleColor		激活主标题字体颜色，默认值#000000
+ * @property {String} 					titleColor				主标题字体颜色和下拉列表中主标题颜色，默认值#000000
+ * @property {String} 					activeTitleColor	激活主标题字体颜色，默认值#000000
+ * @property {String} 					subColor					子标题字体颜色和下拉列表中子标题颜色，默认值#000000
+ * @property {String} 					activeSubColor		激活子标题字体颜色和下拉列表中子标题颜色，默认值#ffffff
+ * @property {Number | String} 	imageWidth				图片宽度，默认80，单位rpx
+ * @property {Number | String} 	imageHeight				图片高度，默认80，单位rpx
+ * @property {Number | String} 	imageRadius				图片圆角，默认50%，单位rpx
+ * @property {Number | String} 	imageBorderWidth	选中图片边框宽度，默认4，单位rpx
  * @property {Boolean} 					showGapLine				是否显示分割线，默认值false
  * @property {Boolean} 					lock							是否锁定（无法切换）
  * @property {Boolean} 					disabled					是否禁用
  * @property {Boolean} 					pullDown					是否有下拉选项（当选项数量大于均分数量时生效）
  * @property {String} 					placeholder				下拉占位符，默认值：请选择
+ * @property {Number | String} 	maskTop						下拉蒙层顶部距离，默认0
+ * @property {Number | String} 	maskRight					下拉蒙层右侧距离，默认0
+ * @property {Number | String} 	maskBottom				下拉蒙层底部距离，默认0
+ * @property {Number | String} 	maskLeft					下拉蒙层左侧距离，默认0
+ * @property {Number} 					maskZindex				下拉蒙层层级，默认1001
  * @event {Function}						change 						选项改变监听,参数{index:number,...item}
  */
 export default {
 	group: '导航组件',
 	title: 'Tabs 标签页',
 	name: 'ste-tabs',
-	props: {
-		// 当前激活的选项支持sync双向绑定（类型为number时绑定index，类型为string时绑定name）
-		active: {
-			type: [Number, String],
-			default: () => 0,
-		},
-		// 样式风格类型，可选值为 line  card
-		type: {
-			type: String,
-			default: () => 'line',
-		},
-		// 是否显示图片
-		showImage: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 是否显示主标题
-		showTitle: {
-			type: Boolean,
-			default: () => true,
-		},
-		// 是否显示副标题
-		showSubtitle: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 主题色（滑块颜色，边框颜色，选中的背景色，激活下拉列表中选项颜色）
-		color: {
-			type: String,
-			default: () => '#0090FF',
-		},
-		// 背景
-		background: {
-			type: String,
-			default: () => '',
-		},
-		// 动画时间，单位秒，设置为 0 可以禁用动画
-		duration: {
-			type: [String, Number],
-			default: () => 0.3,
-		},
-		// 底部条宽度，默认单位rpx
-		lineWidth: {
-			type: [String, Number],
-			default: () => 52,
-		},
-		// 底部条高度，默认单位rpx
-		lineHeight: {
-			type: [String, Number],
-			default: () => 6,
-		},
-		// 是否显示标签栏外边框，仅在 type="line" 时有效
-		border: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 是否省略过长的标题文字
-		ellipsis: {
-			type: Boolean,
-			default: () => false,
-		},
-		// tab内容的宽度
-		tabWidth: {
-			type: [String, Number],
-			default: () => 'auto',
-		},
-		// tab等分数量，设置为 0 ，则不等分
-		divideNum: {
-			type: Number,
-			default: () => 4,
-		},
-		// tab间距rpx
-		tabSpace: {
-			type: [Number, String],
-			default: () => 0,
-		},
-		// 是否使用粘性布局
-		sticky: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 粘性布局下吸顶时与顶部的距离，单位rpx
-		offsetTop: {
-			type: [String, Number],
-			default: () => 0,
-		},
-		// 是否开启手势左右滑动切换
-		swipeable: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 主标题字体颜色和下拉列表中选项颜色
-		titleColor: {
-			type: String,
-			default: () => '#000000',
-		},
-		// 激活主标题字体颜色
-		activeTitleColor: {
-			type: String,
-			default: () => '#000000',
-		},
-		// 是否显示分隔符
-		showGapLine: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 是否锁定tab(无法切换)
-		lock: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 是否禁用tab(所有功能失效)
-		disabled: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 是否有下拉选择按钮
-		pullDown: {
-			type: Boolean,
-			default: () => false,
-		},
-		// 下拉占位符
-		placeholder: {
-			type: String,
-			default: () => '请选择',
-		},
-		tabPadding: {
-			type: [Number, String],
-			default: () => 24,
-		},
-		imageWidth: {
-			type: [Number, String],
-			default: () => 80,
-		},
-		imageHeight: {
-			type: [Number, String],
-			default: () => 80,
-		},
-		imageRadius: {
-			type: [Number, String],
-			default: () => '50%',
-		},
-		imageBorderWidth: {
-			type: [Number, String],
-			default: () => 4,
-		},
-		showLine: {
-			type: Boolean,
-			default: () => true,
-		},
-		subColor: {
-			type: String,
-			default: () => '#000',
-		},
-		activeSubColor: {
-			type: String,
-			default: () => '#fff',
-		},
-		maskTop: {
-			type: [String, Number],
-			default: () => 0,
-		},
-		maskRight: {
-			type: [String, Number],
-			default: () => 0,
-		},
-		maskBottom: {
-			type: [String, Number],
-			default: () => 0,
-		},
-		maskLeft: {
-			type: [String, Number],
-			default: () => 0,
-		},
-		maskZindex: {
-			type: [String, Number],
-			default: () => 1001,
-		},
-	},
-
+	props,
 	provide() {
 		return {
 			_tabsComponent: { getParent: () => this },
