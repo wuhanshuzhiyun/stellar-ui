4a0714b9bf14e5f270f6280f6ee398d4233f5d0b
付宇威 <925720486@qq.com>
2024-03-25 15:16:30 +0800

    输入框

---
+++ mp/input-demo/input-demo.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="page">
+		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="按钮"></page-nav>
+		<view class="content">
+			<view class="demo-item">
+				<view class="title">输入框</view>
+				<view class="item-block">
+					<view style="width: 40%">
+						<ste-input></ste-input>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+export default {
+	data() {
+		return {};
+	},
+	created() {},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+	.content {
+		.demo-item {
+			.item-block {
+				> view {
+					margin: 0 8px 8px 0;
+
+					.loading-icon-box {
+						.iconfont {
+							animation: ste-roatae 1s linear infinite;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@keyframes ste-roatae {
+		0% {
+			transform: rotate(0deg);
+		}
+
+		to {
+			transform: rotate(1turn);
+		}
+	}
+}
+</style>
---
+++ uni_modules/stellar-ui/components/ste-input/ste-input.vue
@@ -0,0 +1,29 @@
+<template>
+	<input class="ste-input--root" placeholder="请输入内容" />
+</template>
+
+<script>
+import utils from '../../utils/utils.js';
+/**
+ * ste-input 输入框
+ * @description 输入框组件
+ * @tutorial https://stellar-ui.intecloud.com.cn//pc/index/index?name=ste-input
+ */
+export default {
+	group: '基础组件',
+	title: 'Input 输入框',
+	name: 'ste-input',
+	props: {},
+	data() {
+		return {};
+	},
+	created() {},
+	computed: {},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.ste-input--root {
+}
+</style>
