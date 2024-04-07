f77965145caabffa628938f9c3cfae9a347eead2
徐亚军 <850674935@qq.com>
2024-04-07 09:06:10 +0800

    feat(save): save

--- pages/code/code.vue
+++ pages/code/code.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="code-page">
+		<!-- #ifdef MP-WEIXIN -->
 		<page-nav :autoBack="false" backColor="#000" titleAlignment="2" title="验证码"></page-nav>
 		<view class="uuid-text">UUID:{{ uuid }}</view>
 		<view class="code-title">验证码</view>
@@ -7,6 +8,7 @@
 		<view style="margin-top: 12rpx">
 			<ste-button mode="300" @click="getCode(uuid)" :loading="loading">刷新验证码</ste-button>
 		</view>
+		<!-- #endif -->
 	</view>
 </template>
 
@@ -32,7 +34,9 @@ export default {
 		}
 		this.getCode(this.uuid);
 	},
-	mounted() {},
+	mounted() {
+		this.getUserInfo();
+	},
 	methods: {
 		getCode(uuid) {
 			if (!this.uuid) return;
@@ -48,6 +52,17 @@ export default {
 					//TODO handle the exception
 					this.loading = false;
 				}
+			});
+			// #endif
+		},
+		getUserInfo() {
+			// #ifdef MP-WEIXIN
+			console.log(":!!!!!!!!")
+			wx.getUserProfile({
+				desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+				success: (res) => {
+					console.log('????????????????');
+				},
 			});
 			// #endif
 		},
