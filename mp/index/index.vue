f8ad809ff1c92b31ee385341cb3516f8040c624b
付宇威 <925720486@qq.com>
2024-04-11 16:32:45 +0800

    预览主页修改

--- mp/index/index.vue
+++ mp/index/index.vue
@@ -1,163 +1,168 @@
 <template>
 	<view class="page">
-		<view class="top">
-			<view class="top-1">
-				<image
-					:src="joinSrc('https://image.whzb.com/chain/StellarUI/logo.png')"
-					style="width: 100rpx; height: 100rpx"
-				/>
-				<view class="title">
-					<view class="title-1">Stellar UI {{ version }}</view>
-					<view class="title-2">微信平台快速开发的UI框架</view>
+		<view class="header">
+			<text>Stellar UI</text>
+		</view>
+		<view class="scroll-box">
+			<view class="top">
+				<view class="top-1">
+					<image
+						:src="joinSrc('https://image.whzb.com/chain/StellarUI/logo.png')"
+						style="width: 100rpx; height: 100rpx"
+					/>
+					<view class="title">
+						<view class="title-1">Stellar UI {{ version }}</view>
+						<view class="title-2">微信平台快速开发的UI框架</view>
+					</view>
+				</view>
+				<view class="top-2">
+					<text>Stellar UI，全面的组件和便捷的工具会让您信手拈来，如鱼得水。</text>
 				</view>
 			</view>
-			<view class="top-2">
-				<text>Stellar UI，全面的组件和便捷的工具会让您信手拈来，如鱼得水。</text>
+			<!-- ***基础组件*** -->
+			<view class="group-title">基础组件</view>
+			<view class="com-box" @click="nav('badge')">
+				<view class="com-title">
+					<image :src="joinSrc('img/badge.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Badge 徽标</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-		</view>
-		<!-- ***基础组件*** -->
-		<view class="group-title">基础组件</view>
-		<view class="com-box" @click="nav('badge')">
-			<view class="com-title">
-				<image :src="joinSrc('img/badge.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Badge 徽标</text>
+			<view class="com-box" @click="nav('button')">
+				<view class="com-title">
+					<image :src="joinSrc('img/button.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Button 按钮</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('button')">
-			<view class="com-title">
-				<image :src="joinSrc('img/button.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Button 按钮</text>
+			<view class="com-box" @click="nav('icon')">
+				<view class="com-title">
+					<image :src="joinSrc('img/icon.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Icon 图标</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('icon')">
-			<view class="com-title">
-				<image :src="joinSrc('img/icon.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Icon 图标</text>
+			<view class="com-box" @click="nav('image')">
+				<view class="com-title">
+					<image :src="joinSrc('img/image.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Image 图片</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('image')">
-			<view class="com-title">
-				<image :src="joinSrc('img/image.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Image 图片</text>
+			<view class="com-box" @click="nav('text')">
+				<view class="com-title">
+					<image :src="joinSrc('img/text.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Text 文本</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('text')">
-			<view class="com-title">
-				<image :src="joinSrc('img/text.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Text 文本</text>
-			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
 
-		<!-- ***表单组件*** -->
-		<view class="group-title">表单组件</view>
-		<view class="com-box" @click="nav('checkbox')">
-			<view class="com-title">
-				<image :src="joinSrc('img/checkbox.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Checkbox 复选框</text>
+			<!-- ***表单组件*** -->
+			<view class="group-title">表单组件</view>
+			<view class="com-box" @click="nav('checkbox')">
+				<view class="com-title">
+					<image :src="joinSrc('img/checkbox.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Checkbox 复选框</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('input')">
-			<view class="com-title">
-				<image :src="joinSrc('img/input.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Input 输入框</text>
+			<view class="com-box" @click="nav('input')">
+				<view class="com-title">
+					<image :src="joinSrc('img/input.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Input 输入框</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('radio')">
-			<view class="com-title">
-				<image :src="joinSrc('img/radio.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Radio 单选框</text>
+			<view class="com-box" @click="nav('radio')">
+				<view class="com-title">
+					<image :src="joinSrc('img/radio.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Radio 单选框</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('rate')">
-			<view class="com-title">
-				<image :src="joinSrc('img/rate.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Rate 评分</text>
+			<view class="com-box" @click="nav('rate')">
+				<view class="com-title">
+					<image :src="joinSrc('img/rate.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Rate 评分</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('rich-text')">
-			<view class="com-title">
-				<image :src="joinSrc('img/parse.png')" style="width: 36rpx; height: 36rpx" />
-				<text>RichText 富文本</text>
+			<view class="com-box" @click="nav('rich-text')">
+				<view class="com-title">
+					<image :src="joinSrc('img/parse.png')" style="width: 36rpx; height: 36rpx" />
+					<text>RichText 富文本</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('search')">
-			<view class="com-title">
-				<image :src="joinSrc('img/search.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Search 搜索</text>
+			<view class="com-box" @click="nav('search')">
+				<view class="com-title">
+					<image :src="joinSrc('img/search.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Search 搜索</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('stepper')">
-			<view class="com-title">
-				<image :src="joinSrc('img/stepper.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Stepper 步进器</text>
+			<view class="com-box" @click="nav('stepper')">
+				<view class="com-title">
+					<image :src="joinSrc('img/stepper.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Stepper 步进器</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('switch')">
-			<view class="com-title">
-				<image :src="joinSrc('img/switch.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Switch 开关</text>
+			<view class="com-box" @click="nav('switch')">
+				<view class="com-title">
+					<image :src="joinSrc('img/switch.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Switch 开关</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('slider')">
-			<view class="com-title">
-				<image :src="joinSrc('img/slider.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Slider 滑块</text>
+			<view class="com-box" @click="nav('slider')">
+				<view class="com-title">
+					<image :src="joinSrc('img/slider.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Slider 滑块</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<!-- ***电商组件*** -->
-		<view class="group-title">电商组件</view>
-		<view class="com-box" @click="nav('price')">
-			<view class="com-title">
-				<image :src="joinSrc('img/price.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Price 价格</text>
+			<!-- ***电商组件*** -->
+			<view class="group-title">电商组件</view>
+			<view class="com-box" @click="nav('price')">
+				<view class="com-title">
+					<image :src="joinSrc('img/price.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Price 价格</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<!-- ***数据组件*** -->
-		<view class="group-title">数据组件</view>
-		<view class="com-box" @click="nav('progress')">
-			<view class="com-title">
-				<image :src="joinSrc('img/progress.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Progress 进度条</text>
+			<!-- ***数据组件*** -->
+			<view class="group-title">数据组件</view>
+			<view class="com-box" @click="nav('progress')">
+				<view class="com-title">
+					<image :src="joinSrc('img/progress.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Progress 进度条</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<!-- ***导航组件*** -->
-		<view class="group-title">导航组件</view>
-		<view class="com-box" @click="nav('tabs')">
-			<view class="com-title">
-				<image :src="joinSrc('img/tabs.png')" style="width: 36rpx; height: 36rpx" />
-				<text>Tabs 标签页</text>
+			<!-- ***导航组件*** -->
+			<view class="group-title">导航组件</view>
+			<view class="com-box" @click="nav('tabs')">
+				<view class="com-title">
+					<image :src="joinSrc('img/tabs.png')" style="width: 36rpx; height: 36rpx" />
+					<text>Tabs 标签页</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('touch-swipe')">
-			<view class="com-title">
-				<image :src="joinSrc('img/countTo.png')" style="width: 36rpx; height: 36rpx" />
-				<text>TouchSwipe 手势切屏</text>
+			<view class="com-box" @click="nav('touch-swipe')">
+				<view class="com-title">
+					<image :src="joinSrc('img/countTo.png')" style="width: 36rpx; height: 36rpx" />
+					<text>TouchSwipe 手势切屏</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
-		</view>
-		<view class="com-box" @click="nav('scroll-to')">
-			<view class="com-title">
-				<image :src="joinSrc('img/tabbar.png')" style="width: 36rpx; height: 36rpx" />
-				<text>ScrollTo 滚动锚点</text>
+			<view class="com-box" @click="nav('scroll-to')">
+				<view class="com-title">
+					<image :src="joinSrc('img/tabbar.png')" style="width: 36rpx; height: 36rpx" />
+					<text>ScrollTo 滚动锚点</text>
+				</view>
+				<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 			</view>
-			<view class="com-arrow"><image :src="joinSrc('img/r-arrow.png')" /></view>
 		</view>
 	</view>
 </template>
@@ -186,8 +191,31 @@ export default {
 <style lang="scss" scoped>
 .page {
 	padding: 120rpx 20px 42px;
+	padding-top: 0;
+	padding-bottom: 0;
 	height: initial;
+
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+	overflow-y: hidden;
 }
+
+.header {
+	height: 80rpx;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	font-size: 40rpx;
+}
+
+.scroll-box {
+	padding-top: 80rpx;
+	flex: 1;
+	overflow-y: auto;
+}
+
 .top {
 	padding-bottom: 28rpx;
 	.top-1 {
