15f344512995ed68c59dce627df9248d5943b2ee
徐亚军 <850674935@qq.com>
2024-04-23 16:42:14 +0800

    feat(save): save

--- uni_modules/stellar-ui/components/ste-swiper-item/ste-swiper-item.vue
+++ uni_modules/stellar-ui/components/ste-swiper-item/ste-swiper-item.vue
@@ -20,14 +20,12 @@ export default {
 			transformX: 0,
 			transformY: 0,
 			transformZ: 0,
-			duration: 0,
 		};
 	},
 	computed: {
 		cmpStyle() {
 			return {
 				transfrom: `translate3d(${this.transformX}px,${this.transformY}px,${this.transformZ}px)`,
-				transitionDuration: `${this.duration}ms`,
 			};
 		},
 	},
@@ -46,11 +44,10 @@ export default {
 		});
 	},
 	methods: {
-		setTransform({ x = 0, y = 0, z = 0, duration = 0 }) {
+		setTransform({ x = 0, y = 0, z = 0 }) {
 			this.transformX = x;
 			this.transformY = y;
 			this.transformZ = z;
-			this.duration = duration;
 		},
 		setIndex(index) {
 			this.index = index;
--- uni_modules/stellar-ui/components/ste-swiper/ste-swiper.vue
+++ uni_modules/stellar-ui/components/ste-swiper/ste-swiper.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="ste-swiper-root" :style="[cmpRootStyle]">
+	<view class="ste-swiper-root" :style="[cmpRootStyle, { opacity: initializing ? 0 : 1 }]">
 		<view
 			class="swipe-content"
 			@mousedown="onTouchstart"
@@ -11,7 +11,7 @@
 			@touchend="onTouchend"
 			@touchcancel="onTouchend"
 		>
-			<view class="swipe-content-view" :style="[cmpBoxStyle]">
+			<view class="swipe-content-view" :style="[cmpBoxStyle, cmpBoxTransform]">
 				<slot></slot>
 			</view>
 		</view>
@@ -48,10 +48,24 @@ export default {
 			type: [Number, String],
 			default: () => null,
 		},
+		disabled: {
+			type: Boolean,
+			default: () => false,
+		},
+		duration: {
+			type: Number,
+			default: () => 300,
+		},
+		swipeThreshold: {
+			type: Number,
+			default: () => 0.35,
+		},
 	},
 	data() {
 		return {
-			
+			initializing: true,
+			moveing: false,
+			dataIndex: 0,
 			childrenComponents: {},
 			childrenData: [],
 			timeout: null,
@@ -66,17 +80,14 @@ export default {
 		cmpRootStyle() {
 			let width = this.direction === 'horizontal' ? '100%' : 'auto';
 			let height = this.direction === 'vertical' ? '100%' : 'auto';
-			if (this.width) {
-				width = utils.formatPx(this.width);
-			}
-			if (this.height) {
-				height = utils.formatPx(this.height);
-			}
-			const style = {
+			if (this.width) width = utils.formatPx(this.width);
+
+			if (this.height) height = utils.formatPx(this.height);
+
+			return {
 				'--swiper-width': width,
 				'--swiper-height': height,
 			};
-			return style;
 		},
 		cmpBoxStyle() {
 			let style = {};
@@ -96,41 +107,80 @@ export default {
 			}
 			return {
 				transform,
-				transitionDuration: !this.init || this.moveing ? 'inherit' : `${this.duration}s`,
+				transitionDuration: this.initializing || this.moveing ? 'inherit' : `${this.duration}ms`,
 			};
 		},
 	},
-	mounted() {
-		setTimeout(async () => {
-			const boxEl = await utils.querySelector('.ste-swiper-root', this);
-			console.log(boxEl);
-			this.boxWidth = boxEl.width;
-			this.boxHeight = boxEl.height;
-		}, 30);
+	watch: {
+		current: {
+			handler(v) {
+				this.dataIndex = v;
+			},
+			immediate: true,
+		},
+		dataIndex: {
+			handler() {
+				this.setTransform();
+			},
+			immediate: true,
+		},
+		childrenData: {
+			handler() {
+				this.setTransform();
+			},
+			immediate: true,
+		},
 	},
+	mounted() {},
 	methods: {
 		getChildren(chil) {
 			this.childrenComponents[chil.index] = chil;
 			clearTimeout(this.timeout);
-			this.timeout = setTimeout(() => {
+			this.timeout = setTimeout(async () => {
+				const boxEl = await utils.querySelector('.swipe-content-view', this);
+				this.boxWidth = boxEl.width;
+				this.boxHeight = boxEl.height;
 				this.childrenData = utils
 					.getChildrenProps(this, 'ste-swiper-item')
 					.map((props, index) => ({ index, component: this.childrenComponents[index] }));
 				this.setTransform();
+				this.$nextTick(() => {
+					this.initializing = false;
+				});
 			}, 30);
 		},
-		setTransform() {
-			console.log('--------setTransform-----------');
+		setTransform(moveX = 0, moveY = 0) {
+			if (!this.childrenData?.length) return;
+			if (this.direction === 'horizontal') this.translateX = -this.dataIndex * this.boxWidth + moveX;
+			if (this.direction === 'vertical') this.translateY = -this.dataIndex * this.boxHeight + moveY;
 		},
 		onTouchstart(e) {
+			if (this.disabled) return;
+			this.moveing = true;
 			this.touch.touchStart(e);
 		},
 		onTouchmove(e) {
+			if (this.disabled) return;
+			this.moveing = true;
 			const { moveX, moveY } = this.touch.touchMove(e);
-			console.log(moveX, moveY);
+			this.setTransform(moveX, moveY);
 		},
 		onTouchend(e) {
+			this.moveing = false;
 			const { moveX, moveY } = this.touch.touchEnd(e);
+			if (this.direction === 'horizontal' && !moveX) return;
+			else if (this.direction === 'vertical' && !moveY) return;
+			let index = this.dataIndex;
+			if (this.direction === 'horizontal' && Math.abs(moveX) > this.boxWidth * this.swipeThreshold) {
+				index = moveX > 0 ? index - 1 : index + 1;
+			} else if (this.direction === 'vertical' && Math.abs(moveY) > this.boxHeight * this.swipeThreshold) {
+				index = moveY > 0 ? index - 1 : index + 1;
+			}
+			if (this.dataIndex === index) {
+				this.setTransform();
+				return;
+			}
+			this.dataIndex = index;
 		},
 	},
 };
