4eb3ce12dad36344d136a1813a93be881a01cb73
秦鹏飞 <q1506589520q@gmail.com>
2024-05-09 17:16:15 +0800

    加载组件完成

---
+++ uni_modules/stellar-ui/components/ste-loading/README.md
@@ -0,0 +1,79 @@
+# Loading 加载
+
+加载动画，用于表示加载中的过渡状态。
+
+{{compatibility}}
+
+## 代码演示
+
+### 加载类型
+通过 `type`设置加载动画类型，默认为类型`1`
+- `1`：逐渐减淡的颜色为主色的透明度变化 透明度以10%的差距逐渐递减
+- `2`：动态转圈展示
+
+```html
+<ste-loading></ste-loading>
+<ste-loading :type="2"></ste-loading>
+```
+
+### 自定义图标颜色
+通过 `color` 属性来设置图标的颜色
+
+```html
+<ste-loading color="#0091FF"></ste-loading>
+<ste-loading :type="2" color="#0091FF"></ste-loading>
+```
+
+### 自定义图标大小
+通过 `size` 属性来设置图标的尺寸大小，单位为 rpx，默认`60`
+
+```html
+<ste-loading :size="100"></ste-loading>
+<ste-loading :type="2" :size="100"></ste-loading>
+```
+
+### 加载文案
+默认插槽`default`为加载文案
+```html
+<ste-loading>加载中...</ste-loading>
+<ste-loading :type="2">加载中...</ste-loading>
+```
+
+### 垂直排列
+通过 `vertical` 是否垂直排列图标和文字内容，默认`false`
+
+```html
+<ste-loading vertical>加载中...</ste-loading>
+<ste-loading :type="2" vertical>加载中...</ste-loading>
+```
+
+### 自定义文本样式
+- 通过 `textSize`设置文本大小，单位rpx，默认为`28`
+- 通过 `textColor`设置文本颜色，默认和图标颜色同步
+
+```html
+<ste-loading :textSize="50" textColor="#0091FF">加载中...</ste-loading>
+<ste-loading :type="2" :textSize="50" textColor="#0091FF">加载中...</ste-loading>
+```
+
+### API
+
+#### Props
+
+| 属性名		| 说明						| 类型		| 默认值		| 可选值																			| 支持版本	|
+| -----			| -----						| -----		| -----		| -----																			| -----		|
+| `type`		| 类型						| `Number`	| `1`		| `1`：逐渐减淡的颜色为主色的透明度变化 透明度以10%的差距逐渐递减 <br>`2`：动态转圈展示	| -			|
+| `color`		| 图标颜色					| `String`	| `#999999`	| -																				| -			|
+| `size`		| 图标大小，单位rpx			| `Number`	| `60`		| -																				| -			|
+| `textColor`	| 文本颜色，默认和图标颜色同步	| `String`	| -			| -																				| -			|
+| `textSize`	| 文本大小，单位rpx			| `Number`	| `28`		| -																				| -			|
+| `vertical`	| 是否垂直排列图标和文字内容	| `Boolean`	| `false`	| -																				| -			|
+
+#### 组件插槽(Slots)
+
+|插槽名		|说明		|插槽参数	|支持版本	|
+|---		|---		|---		|---		|
+| `default`	| 	加载文案	| -			| -			|
+
+
+{{qinpengfei}}
--- uni_modules/stellar-ui/components/ste-loading/ste-loading.vue
+++ uni_modules/stellar-ui/components/ste-loading/ste-loading.vue
@@ -1,21 +1,37 @@
 <template>
-	<view class="ste-loading-root" :style="[cmpStyle]"></view>
+	<view class="ste-loading-root" :style="[cmpStyle]">
+		<view :class="'loading ' + 'loading-type-' + type" :style="[cmpLoadinStyle]">
+			<block v-if="type == 1">
+				<i
+					v-for="item in cmpCount"
+					class="i"
+					:style="{ transform: `rotate(${item * 40 + 80}deg)`, opacity: item == 0 ? 1 : (item + 1) / 10 }"
+				></i>
+			</block>
+			<block v-if="type == 2">
+				<view></view>
+			</block>
+		</view>
+		<view class="text" :style="[cmpText]">
+			<slot></slot>
+		</view>
+	</view>
 </template>
 
 <script>
 import utils from '../../utils/utils.js';
 /**
- * ste-loading 加载动画
- * @description 加载动画组件
+ * ste-loading 加载
+ * @description 加载组件，用于表示加载中的过渡状态。
  * @tutorial https://stellar-ui.intecloud.com.cn/pc/index/index?name=ste-loading
  * @property {String} color 图标颜色，默认#999999
- * @property {String} text 提示文字内容
  * @property {String} textColor 提示文本颜色，默认和图标颜色同步
  * @property {Number} textSize 文本大小，单位rpx，默认28
  * @property {Number} type 类型，默认1
- * @value 1  类型1  逐渐减淡的颜色为主色的透明度变化 透明度以10%的差距逐渐递减{{Number}}
- * @value 2  类型2  动态转圈展示{{Number}}
- * @property {Number} size 大小，单位rpx，默认60
+ * @value 1  逐渐减淡的颜色为主色的透明度变化 透明度以10%的差距逐渐递减{{Number}}
+ * @value 2  动态转圈展示{{Number}}
+ * @property {Number} size 图标大小，单位rpx，默认60
+ * @property {Boolean} vertical  是否垂直排列图标和文字内容，默认false
  */
 export default {
 	group: '展示组件',
@@ -25,10 +41,6 @@ export default {
 		color: {
 			type: String,
 			default: () => '#999999',
-		},
-		text: {
-			type: String,
-			default: () => '',
 		},
 		textColor: {
 			type: String,
@@ -46,12 +58,48 @@ export default {
 			type: Number,
 			default: () => 60,
 		},
+		vertical: {
+			type: Boolean,
+			default: () => false,
+		},
+	},
+	data() {
+		return {
+			count: 9,
+		};
 	},
 	computed: {
 		cmpStyle() {
 			let style = {};
+			style['flexDirection'] = this.vertical ? 'column' : 'row';
+			if (this.vertical) {
+				style['rowGap'] = utils.formatPx(16);
+			} else {
+				style['columnGap'] = utils.formatPx(16);
+			}
+			return style;
+		},
+		cmpLoadinStyle() {
+			let style = {};
 			style['width'] = utils.formatPx(this.size);
 			style['height'] = utils.formatPx(this.size);
+			style['color'] = this.color;
+			if (this.type == 1) {
+				style['animationTimingFunction'] = 'steps(8)';
+			}
+			if (this.type == 2) {
+				style['borderColor'] = (this.color + ' ').repeat(3) + 'transparent';
+			}
+			return style;
+		},
+		cmpCount() {
+			// 兼容浏览器和微信 对数字循环的处理不同
+			return Array.from({ length: this.count }, (_, index) => index);
+		},
+		cmpText() {
+			let style = {};
+			style['color'] = this.textColor ? this.textColor : this.color;
+			style['fontSize'] = utils.formatPx(this.textSize);
 			return style;
 		},
 	},
@@ -61,5 +109,61 @@ export default {
 <style lang="scss" scoped>
 .ste-loading-root {
 	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+
+	.loading {
+		position: relative;
+		display: inline-block;
+		max-width: 100%;
+		max-height: 100%;
+		vertical-align: middle;
+
+		.i {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+		.i:before {
+			display: block;
+			width: 12%;
+			height: 25%;
+			margin: 0 auto;
+			background-color: currentColor;
+			border-radius: 40%;
+			content: ' ';
+		}
+	}
+
+	.loading-type-1 {
+		animation: ste-rotate 0.8s linear infinite;
+	}
+	.loading-type-2 {
+		width: 100%;
+		height: 100%;
+		border: 3rpx solid;
+		border-radius: 50%;
+		animation: ste-circular 1s linear infinite;
+	}
+
+	@keyframes ste-rotate {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	@keyframes ste-circular {
+		0% {
+			transform: rotate(0);
+		}
+		100% {
+			transform: rotate(1turn);
+		}
+	}
 }
 </style>
