3d951a34620fc94b2afe144217b8692a5466f287
徐亚军 <850674935@qq.com>
2024-05-13 14:41:14 +0800

    feat(save): 优化UUID生成功能，添加system部分方法

---
+++ uni_modules/stellar-ui/utils/System.js
@@ -0,0 +1,33 @@
+export default class System {
+	static systemInfo = uni.getSystemInfoSync();
+	static navbarBottom = null;
+	static windowWidth = null;
+
+	/**
+	 * 获取屏幕宽度
+	 */
+	static getWindowWidth() {
+		if (System.windowWidth === null) {
+			System.windowWidth = System.systemInfo.windowWidth;
+		}
+		return System.windowWidth;
+	}
+
+	/**
+	 * 获取手机顶部安全区域距离顶部的距离（状态栏高度）
+	 */
+	static getStatusBarHeight() {
+		return System.systemInfo.statusBarHeight;
+	}
+	
+	/**
+	 * 获取导航栏底部安全区域距离底部的距离（底部安全区距离状态栏的距离）
+	 */
+	static getNavbarBottom() {
+		if (System.navbarBottom === null) {
+			const menuButtonInfo = uni.getMenuButtonBoundingClientRect();
+			System.navbarBottom = menuButtonInfo + 8;
+		}
+		return System.navbarBottom;
+	}
+}
--- uni_modules/stellar-ui/utils/utils.js
+++ uni_modules/stellar-ui/utils/utils.js
@@ -1,10 +1,11 @@
 import Color from './Color.js';
+import System from './System.js';
 let throLast = 0; // 节流方法用变量
 let throTimer = null; // 节流方法用的变量
-let windowWidth = null;
 
 let utils = {
 	Color,
+	System,
 	/**
 	 * 格式化像素单位为px
 	 * @param value {Number | String} 像素单位值
@@ -14,7 +15,7 @@ let utils = {
 		let format = value ? value : 0;
 		if (format && isNaN(format)) {
 			if (/^\d+px$/i.test(format)) {
-				return format;
+				return restype ? Number(format.slice(0, -2)) : format;
 			} else if (/^\d+rpx/i.test(format)) {
 				format = Number(format.slice(0, -3));
 			} else {
@@ -23,10 +24,7 @@ let utils = {
 		}
 		let px = 0;
 		if (format !== 0) {
-			if (windowWidth == null) {
-				windowWidth = uni.getSystemInfoSync().windowWidth;
-			}
-			px = (format * windowWidth) / 750;
+			px = (format * System.getWindowWidth()) / 750;
 		}
 		return restype === 'num' ? px : `${px}px`;
 	},
@@ -141,7 +139,8 @@ let utils = {
 		const selectFn = all ? 'selectAll' : 'select';
 		return new Promise((resolve, reject) => {
 			try {
-				uni.createSelectorQuery()
+				uni
+					.createSelectorQuery()
 					.in(component)
 					[selectFn](selectors)
 					.boundingClientRect((data) => {
@@ -157,7 +156,7 @@ let utils = {
 		viewLeft, // 要显示的元素左侧位置
 		viewRight, // 要显示的元素右侧位置
 		boxLeft = 0, // 视图区域左侧位置
-		boxRight = uni.getSystemInfoSync().windowWidth, // 视图区域右侧位置
+		boxRight = System.getWindowWidth(), // 视图区域右侧位置
 		prevWidth = 0, // 前一个元素的宽度
 		nextWidth = 0, // 后一个元素的宽度
 		scrollLeft = 0, // 当前已经滑动的距离
@@ -286,44 +285,13 @@ let utils = {
 		});
 	},
 	/**
-	 * 本算法来源于简书开源代码，详见：https://www.jianshu.com/p/fdbf293d0a85
-	 * 全局唯一标识符（uuid，Globally Unique Identifier）,也称作 uuid(Universally Unique IDentifier)
-	 * 一般用于多个组件之间,给它一个唯一的标识符,或者v-for循环的时候,如果使用数组的index可能会导致更新列表出现问题
-	 * 最可能的情况是左滑删除item或者对某条信息流"不喜欢"并去掉它的时候,会导致组件内的数据可能出现错乱
-	 * v-for的时候,推荐使用后端返回的id而不是循环的index
+	 * 全局唯一标识符
 	 * @param {Number} len uuid的长度
-	 * @param {Boolean} firstU 将返回的首字母置为"u"
-	 * @param {Nubmer} radix 生成uuid的基数(意味着返回的字符串都是这个基数),2-二进制,8-八进制,10-十进制,16-十六进制
+	 * @param {Boolean} firstU 将返回的首字母置为"u
 	 */
-	guid(len = 32, firstU = true, radix = null) {
-		let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-		let uuid = [];
-		radix = radix || chars.length;
-
-		if (len) {
-			// 如果指定uuid长度,只是取随机的字符,0|x为位运算,能去掉x的小数位,返回整数位
-			for (let i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
-		} else {
-			let r;
-			// rfc4122标准要求返回的uuid中,某些位为固定的字符
-			uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-			uuid[14] = '4';
-
-			for (let i = 0; i < 36; i++) {
-				if (!uuid[i]) {
-					r = 0 | (Math.random() * 16);
-					uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r];
-				}
-			}
-		}
-		// 移除第一个字符,并用u替代,因为第一个字符为数值时,该guuid不能用作id或者class
-		let str = '';
-		if (firstU) {
-			uuid.shift();
-			str = 'u' + uuid.join('');
-		} else {
-			str = uuid.join('');
-		}
+	guid(len = 32, firstU = true) {
+		let str = firstU ? 'u' : '';
+		for (let i = str.length; i < len; i++) str += Math.floor(Math.random() * 32).toString(32);
 		return str;
 	},
 	/**
