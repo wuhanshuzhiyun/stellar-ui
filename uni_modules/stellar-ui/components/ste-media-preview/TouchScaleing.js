566a8b78839c2b4fae6525e9c00f321ee3b7fd17
徐亚军 <850674935@qq.com>
2024-05-14 11:48:47 +0800

    feat(save): 预览缩放添加对苹果手机的支持

--- uni_modules/stellar-ui/components/ste-media-preview/TouchScaleing.js
+++ uni_modules/stellar-ui/components/ste-media-preview/TouchScaleing.js
@@ -1,4 +1,6 @@
 export default class TouchScaleing {
+	identifiers = [];
+
 	startParam = {
 		x1: null,
 		y1: null,
@@ -13,19 +15,22 @@ export default class TouchScaleing {
 	translateX = 0;
 	translateY = 0;
 
-	_getTouchPosition(changedTouches) {
-		const x1 =
-			changedTouches.find((item) => item.identifier === 0)?.pageX || null;
-		const y1 =
-			changedTouches.find((item) => item.identifier === 0)?.pageY || null;
-		const x2 =
-			changedTouches.find((item) => item.identifier === 1)?.pageX || null;
-		const y2 =
-			changedTouches.find((item) => item.identifier === 1)?.pageY || null;
+	_getTouchPosition() {
+		const [m1, m2] = this.identifiers;
+		const x1 = m1?.pageX || null;
+		const y1 = m1?.pageY || null;
+		const x2 = m2?.pageX || null;
+		const y2 = m2?.pageY || null;
 		return [x1, y1, x2, y2];
 	}
 	touchStart(changedTouches) {
-		const [x1, y1, x2, y2] = this._getTouchPosition(changedTouches);
+		if (this.identifiers.length < 2) {
+			changedTouches.forEach((item) => {
+				if (this.identifiers.findIndex((m) => m.identifier === item.identifier) === -1) this.identifiers.push(item);
+			});
+		}
+
+		const [x1, y1, x2, y2] = this._getTouchPosition();
 		if (x1 !== null && y1 !== null) {
 			this.startParam.x1 = x1;
 			this.startParam.y1 = y1;
@@ -36,21 +41,18 @@ export default class TouchScaleing {
 		}
 	}
 	touchMove(changedTouches) {
-		if (changedTouches.length < 2) return false;
+		if (this.identifiers.length < 2) return false;
+		this.identifiers.forEach((item) => {
+			const newData = changedTouches.find((item2) => item2.identifier === item.identifier);
+			if (newData) Object.assign(item, newData);
+		});
 		const param = this.startParam;
-		const [startX1, startY1, startX2, startY2] = [
-			param.x1,
-			param.y1,
-			param.x2,
-			param.y2,
-		];
-		const [x1, y1, x2, y2] = this._getTouchPosition(changedTouches);
+		const [startX1, startY1, startX2, startY2] = [param.x1, param.y1, param.x2, param.y2];
+		const [x1, y1, x2, y2] = this._getTouchPosition();
 
 		this.scale =
 			Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) /
-			Math.sqrt(
-				Math.pow(startX2 - startX1, 2) + Math.pow(startY2 - startY1, 2)
-			);
+			Math.sqrt(Math.pow(startX2 - startX1, 2) + Math.pow(startY2 - startY1, 2));
 		this.rotate =
 			(Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI -
 			(Math.atan2(startY2 - startY1, startX2 - startX1) * 180) / Math.PI;
@@ -59,7 +61,13 @@ export default class TouchScaleing {
 		return true;
 	}
 	touchEnd(changedTouches) {
-		const [x1, y1, x2, y2] = this._getTouchPosition(changedTouches);
+		changedTouches.forEach((item) => {
+			const index = this.identifiers.findIndex((m) => m.identifier === item.identifier);
+			if (index !== -1) {
+				this.identifiers.splice(index, 1);
+			}
+		});
+		const [x1, y1, x2, y2] = this._getTouchPosition();
 		if (x1 !== null && y1 !== null) {
 			this.startParam.x1 = null;
 			this.startParam.y1 = null;
@@ -68,11 +76,6 @@ export default class TouchScaleing {
 			this.startParam.x2 = null;
 			this.startParam.y2 = null;
 		}
-		return (
-			this.startParam.x1 === null &&
-			this.startParam.y1 === null &&
-			this.startParam.x2 === null &&
-			this.startParam.y2 === null
-		);
+		return !this.identifiers.lenth;
 	}
 }
