8a262642676558133ee71189a3612ed036b5aaa2
付宇威 <925720486@qq.com>
2024-05-22 16:53:22 +0800

    阅读更多组件

---
+++ mp/read-more-demo/read-more-demo.vue
@@ -0,0 +1,80 @@
+<template>
+	<view class="page">
+		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="阅读更多"></page-nav>
+		<view class="content">
+			<view class="demo-item">
+				<view class="title">基础使用</view>
+				<view class="item-block">
+					<view>
+						<ste-read-more>{{ moreStr }}</ste-read-more>
+					</view>
+					<view>
+						<ste-read-more>{{ moreStr2 }}</ste-read-more>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">收起后展示按钮</view>
+				<view class="item-block">
+					<view>
+						<ste-read-more toggle>{{ moreStr }}</ste-read-more>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">提示文字</view>
+				<view class="item-block">
+					<view>
+						<ste-read-more toggle openText="收起吧" closeText="展开吧" color="#f1f" fontSize="32">
+							{{ moreStr }}
+						</ste-read-more>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">异步初始化</view>
+				<view class="item-block">
+					<view>
+						<ste-read-more ref="steReadMore">{{ moreStr3 }}</ste-read-more>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			moreStr:
+				'浔阳江头夜送客，枫叶荻花秋瑟瑟。主人下马客在船，举酒欲饮无管弦。醉不成欢惨将别，别时茫茫江浸月。 忽闻水上琵琶声，主人忘归客不发。寻声暗问弹者谁，琵琶声停欲语迟。移船相近邀相见，添酒回灯重开宴。千呼万唤始出来，犹抱琵琶半遮面。凝绝不通声暂歇。悄无言，唯见江心秋月白。 沉吟放拨插弦中，整顿衣裳起敛容。自言本是京城女，家在虾蟆陵下住。十三学得琵琶成，名属教坊第一部。曲罢曾教善才服，妆成每被秋娘妒。五陵年少争缠头，一曲红绡不知数。钿头银篦击节碎，血色罗裙翻酒污。今年欢笑复明年，秋月春风等闲度。弟走从军阿姨死，暮去朝来颜色故',
+			moreStr2:
+				'浔阳江头夜送客，枫叶荻花秋瑟瑟。主人下马客在船，举酒欲饮无管弦。醉不成欢惨将别，别时茫茫江浸月。 忽闻水上琵琶声,浔阳江头夜送客，枫叶荻花秋瑟瑟。主人下马客在船，举酒欲饮无管弦。醉不成欢惨将别，别时茫茫江浸月。 忽闻水上琵琶声',
+			moreStr3: '',
+		};
+	},
+	created() {
+		setTimeout(() => {
+			this.moreStr3 = this.moreStr;
+			this.$nextTick(() => {
+				this.$refs.steReadMore.init();
+			});
+		}, 1500);
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+	.content {
+		.demo-item {
+			.item-block {
+				> view {
+					// margin: 0 36px 36px 0;
+					margin-bottom: 36rpx;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ uni_modules/stellar-ui/components/ste-read-more/ste-read-more.vue
@@ -0,0 +1,119 @@
+<template>
+	<view class="ste-read-more-root">
+		<view class="content-box" :style="[cmpContentStyle]">
+			<view class="content">
+				<slot></slot>
+			</view>
+		</view>
+		<view class="action-box" :style="[cmpActionStyle]" v-if="cmpShowAction">
+			<text @click="handleToggleStatus">{{ open ? openText : closeText }}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+import utils from '../../utils/utils.js';
+export default {
+	group: '展示组件',
+	title: 'ReadMore 阅读更多',
+	name: 'ste-read-more',
+	props: {
+		showHeight: {
+			type: [String, Number],
+			default: 400,
+		},
+		// 展开后是否显示收起按钮
+		toggle: {
+			type: Boolean,
+			default: false,
+		},
+		closeText: {
+			type: String,
+			default: '展开阅读全文',
+		},
+		openText: {
+			type: String,
+			default: '收起',
+		},
+		fontSize: {
+			type: [String, Number],
+			default: 24,
+		},
+		color: {
+			type: String,
+			default: '#2979ff',
+		},
+	},
+	data() {
+		return {
+			open: false,
+			showToggle: false,
+		};
+	},
+	watch: {},
+	computed: {
+		cmpContentStyle() {
+			let style = {};
+			if (this.showToggle) {
+				style.height = this.open ? 'auto' : utils.addUnit(this.showHeight);
+			}
+
+			return style;
+		},
+		cmpActionStyle() {
+			let style = {
+				color: this.color,
+				fontSize: utils.addUnit(this.fontSize),
+				background: this.open
+					? ''
+					: 'linear-gradient(-180deg, rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 80%)',
+			};
+			return style;
+		},
+		cmpShowAction() {
+			let show = true;
+			if (!this.showToggle) {
+				show = false;
+			}
+
+			if (this.open && !this.toggle) {
+				show = false;
+			}
+
+			return show;
+		},
+	},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			utils.querySelector('.content', this).then((rect) => {
+				console.log('rect is ', rect);
+				this.showToggle = parseInt(rect.height) > parseInt(utils.formatPx(this.showHeight));
+			});
+		},
+		handleToggleStatus() {
+			this.open = !this.open;
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.ste-read-more-root {
+	.content-box {
+		overflow: hidden;
+	}
+
+	.action-box {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		position: relative;
+		padding-top: 200rpx;
+		margin-top: -200rpx;
+	}
+}
+</style>
