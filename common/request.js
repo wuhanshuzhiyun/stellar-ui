932dd9680ad2210e0dcea9994fdada00daa03234
徐亚军 <850674935@qq.com>
2024-05-27 09:28:54 +0800

    feat(save): 添加请求打印

--- common/request.js
+++ common/request.js
@@ -1,22 +1,24 @@
 import config from './config.js';
-export default function(url, data, method = "GET") {
+console.log(config.BASE_URL);
+export default function (url, data, method = 'GET') {
 	return new Promise((resolve, reject) => {
+		console.log('----request-start==>', url);
 		uni.request({
 			url: `${config.BASE_URL}${url}`,
 			method,
 			data,
-			success({
-				data
-			}) {
+			success({ data }) {
+				console.log('----request-success==>', url, data);
 				if (data.code === 0) {
-					resolve(data.data)
-					return
+					resolve(data.data);
+					return;
 				}
-				reject(data.message)
+				reject(data.message);
 			},
 			fail(e) {
-				reject(e)
-			}
+				console.log('----request-error==>', url, e);
+				reject(e);
+			},
 		});
-	})
+	});
 }
