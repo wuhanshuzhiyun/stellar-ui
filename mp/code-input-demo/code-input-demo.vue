42a6fa9a17a18fbd45e4931e32db801a9b41cf5c
付宇威 <925720486@qq.com>
2024-05-29 15:36:30 +0800

    修改样式

--- mp/code-input-demo/code-input-demo.vue
+++ mp/code-input-demo/code-input-demo.vue
@@ -15,6 +15,86 @@
 					</view>
 				</view>
 			</view>
+			<view class="demo-item">
+				<view class="title">横线模式</view>
+				<view class="item-block">
+					<view>
+						<ste-code-input v-model="value2" mode="line" :maxlength="4" :bold="true"></ste-code-input>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">设置长度</view>
+				<view class="item-block">
+					<view>
+						<ste-code-input v-model="value3" :maxlength="5"></ste-code-input>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">设置间距</view>
+				<view class="item-block">
+					<view>
+						<ste-code-input v-model="value4" mode="box" :space="0" :maxlength="4"></ste-code-input>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">调整颜色</view>
+				<view class="item-block">
+					<view style="width: 100%">
+						<ste-code-input
+							v-model="value7"
+							mode="box"
+							:space="0"
+							:maxlength="4"
+							fontColor="#f56c6c"
+							borderColor="#f56c6c"
+						></ste-code-input>
+					</view>
+					<view style="width: 100%">
+						<ste-code-input
+							v-model="value10"
+							mode="line"
+							:maxlength="4"
+							fontColor="#3c9cff"
+							borderColor="#3c9cff"
+						></ste-code-input>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">自定义显示</view>
+				<view class="item-block">
+					<view style="width: 100%">
+						<ste-code-input
+							v-model="value8"
+							mode="box"
+							formatter="·"
+							fontSize="100"
+							:space="0"
+							:maxlength="4"
+						></ste-code-input>
+					</view>
+					<view style="width: 100%">
+						<ste-code-input
+							v-model="value11"
+							mode="box"
+							formatter="*"
+							:space="0"
+							:maxlength="4"
+						></ste-code-input>
+					</view>
+				</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">预置内容</view>
+				<view class="item-block">
+					<view>
+						<ste-code-input v-model="value9" mode="box" :space="0" :maxlength="4"></ste-code-input>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -23,6 +103,16 @@ export default {
 	data() {
 		return {
 			value1: '',
+			value2: '',
+			value3: '',
+			value4: '',
+			value5: '',
+			value6: '',
+			value7: '',
+			value8: '',
+			value9: '123',
+			value10: '34',
+			value11: '',
 		};
 	},
 	created() {},
