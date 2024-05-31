cdcff41802fed303307d8a88ab7f52867489459a
秦鹏飞 <q1506589520q@gmail.com>
2024-05-31 15:06:40 +0800

    修改图片地址指向

--- mp/index/index.vue
+++ mp/index/index.vue
@@ -24,305 +24,309 @@
 			<view class="group-title">基础组件</view>
 			<view class="com-box" @click="nav('badge')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/badge.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/badge.png')" :width="36" :height="36" />
 					<text>Badge 徽标</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('button')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/button.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/button.png')" :width="36" :height="36" />
 					<text>Button 按钮</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('icon')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/icon.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/icon.png')" :width="36" :height="36" />
 					<text>Icon 图标</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('image')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/image.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/image.png')" :width="36" :height="36" />
 					<text>Image 图片</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('popup')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/popup.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/popup.png')" :width="36" :height="36" />
 					<text>Popup 弹出层</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('sticky')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/sticky.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/sticky.png')" :width="36" :height="36" />
 					<text>Sticky 吸顶</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('text')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/text.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/text.png')" :width="36" :height="36" />
 					<text>Text 文本</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<!-- ***表单组件*** -->
 			<view class="group-title">表单组件</view>
 			<view class="com-box" @click="nav('calendar')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/calendar.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/calendar.png')" :width="36" :height="36" />
 					<text>Calendar 日历</text>
 				</view>
-				<view class="com-arrow"><ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" /></view>
+				<view class="com-arrow">
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
+				</view>
 			</view>
 			<view class="com-box" @click="nav('checkbox')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/checkbox.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/checkbox.png')" :width="36" :height="36" />
 					<text>Checkbox 复选框</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('code-input')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/code-input.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/code-input.png')" :width="36" :height="36" />
 					<text>CodeInput 验证码输入</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('date-picker')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/datetime.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/datetime.png')" :width="36" :height="36" />
 					<text>DatePicker 时间选择</text>
 				</view>
-				<view class="com-arrow"><ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" /></view>
+				<view class="com-arrow">
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
+				</view>
 			</view>
 			<view class="com-box" @click="nav('input')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/input.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/input.png')" :width="36" :height="36" />
 					<text>Input 输入框</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('radio')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/radio.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/radio.png')" :width="36" :height="36" />
 					<text>Radio 单选框</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('rate')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/rate.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/rate.png')" :width="36" :height="36" />
 					<text>Rate 评分</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('rich-text')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/parse.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/parse.png')" :width="36" :height="36" />
 					<text>RichText 富文本</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('search')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/search.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/search.png')" :width="36" :height="36" />
 					<text>Search 搜索</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('slider')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/slider.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/slider.png')" :width="36" :height="36" />
 					<text>Slider 滑块</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('stepper')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/stepper.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/stepper.png')" :width="36" :height="36" />
 					<text>Stepper 步进器</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('switch')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/switch.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/switch.png')" :width="36" :height="36" />
 					<text>Switch 开关</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('upload')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/upload.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/upload.png')" :width="36" :height="36" />
 					<text>Upload 上传</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<!-- ***导航组件*** -->
 			<view class="group-title">导航组件</view>
 			<view class="com-box" @click="nav('dropdown-menu')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/dropdown.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/dropdown.png')" :width="36" :height="36" />
 					<text>DropdownMenu 下拉菜单</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('index-list')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('indexList.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/indexList.png')" :width="36" :height="36" />
 					<text>IndexList 索引列表</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('scroll-to')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/tabbar.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/tabbar.png')" :width="36" :height="36" />
 					<text>ScrollTo 滚动锚点</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('swiper')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/countTo.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/countTo.png')" :width="36" :height="36" />
 					<text>Swipe 轮播组件</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('tabs')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/tabs.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/tabs.png')" :width="36" :height="36" />
 					<text>Tabs 标签页</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('touch-swipe')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/countTo.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/countTo.png')" :width="36" :height="36" />
 					<text>TouchSwipe 手势切屏</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<!-- ***展示组件*** -->
 			<view class="group-title">展示组件</view>
 			<view class="com-box" @click="nav('loading')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/loading.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/loading.png')" :width="36" :height="36" />
 					<text>Loading 加载动画</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('media-preview')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/mediaPreview.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/mediaPreview.png')" :width="36" :height="36" />
 					<text>MediaPreview 媒体预览</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('notice-bar')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/noticeBar.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/noticeBar.png')" :width="36" :height="36" />
 					<text>NoticeBar 公告栏</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('read-more')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/section.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/section.png')" :width="36" :height="36" />
 					<text>ReadMore 阅读更多</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<view class="com-box" @click="nav('swipe-action')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/countTo.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/countTo.png')" :width="36" :height="36" />
 					<text>SwipeAction 滑动单元格</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<!-- ***电商组件*** -->
 			<view class="group-title">电商组件</view>
 			<view class="com-box" @click="nav('price')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/price.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/price.png')" :width="36" :height="36" />
 					<text>Price 价格</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 			<!-- ***数据组件*** -->
 			<view class="group-title">数据组件</view>
 			<view class="com-box" @click="nav('progress')">
 				<view class="com-title">
-					<ste-image :src="joinSrc('img/progress.png')" :width="36" :height="36" />
+					<ste-image :src="joinSrc('组件图标/progress.png')" :width="36" :height="36" />
 					<text>Progress 进度条</text>
 				</view>
 				<view class="com-arrow">
-					<ste-image :src="joinSrc('img/r-arrow.png')" :width="32" :height="32" />
+					<ste-image :src="joinSrc('组件图标/r-arrow.png')" :width="32" :height="32" />
 				</view>
 			</view>
 		</view>
