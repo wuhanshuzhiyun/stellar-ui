5439e73c7972295ea00cbeec0d56ce7bb742ab9b
付宇威 <f6512098@163.com>
2024-06-26 16:00:19 +0800

    弹框组件

---
+++ mp/message-box-demo/message-box-demo.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="page">
+		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="弹框"></page-nav>
+		<view class="content">
+			<view class="demo-item">
+				<view class="title">基础使用</view>
+				<view class="item-block">
+					<view></view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+export default {
+	data() {
+		return {};
+	},
+	created() {},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+	.content {
+		.demo-item {
+			.item-block {
+				> view {
+					margin: 0 8px 8px 0;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ uni_modules/stellar-ui/components/ste-message-box/ste-message-box.vue
@@ -0,0 +1,18 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import utils from '../../utils/utils.js';
+export default {
+	group: '展示组件',
+	title: 'MessageBox 弹框',
+	name: 'ste-message-box',
+	data() {
+		return {};
+	},
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
