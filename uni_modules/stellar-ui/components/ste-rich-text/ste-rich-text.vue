a59896e817de76751c8dd32166a1cbc63e4f9797
秦鹏飞 <q1506589520q@gmail.com>
2024-08-13 10:43:08 +0800

    富文本，增加文本换行功能

--- uni_modules/stellar-ui/components/ste-rich-text/ste-rich-text.vue
+++ uni_modules/stellar-ui/components/ste-rich-text/ste-rich-text.vue
@@ -50,6 +50,9 @@ export default {
 			html = utils.richTextTagAddStyle(html, 'p', ['margin', 'margin-top'], ';margin-top:0');
 			html = utils.richTextTagAddStyle(html, 'p', ['margin', 'margin-bottom'], ';margin-bottom:0');
 			html = utils.richTextTagAddStyle(html, 'img', 'vertical-align', ';vertical-align:top');
+			// 解决微信小程序会忽略空白换行的问题
+			html = html.replace(/↵/g, '\n');
+			html = html.replace(/&nbsp;/g, '\n');
 			return html;
 		},
 	},
@@ -57,4 +60,9 @@ export default {
 	mounted() {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.rich-text {
+	word-break: break-all;
+	white-space: pre-line;
+}
+</style>
