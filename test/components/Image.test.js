517c33ef3226ca8124be7aca170172ba5e70dedd
徐亚军 <850674935@qq.com>
2025-02-10 10:49:15 +0800

    feat(image): 测试

--- test/components/Image.test.js
+++ test/components/Image.test.js
@@ -1,23 +1,42 @@
-import { expect, test, vi } from 'vitest';
+import { expect, test, vi, describe } from 'vitest';
 import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import Image from '../../uni_modules/stellar-ui/components/ste-image/ste-image.vue';
 import { style2obj } from '../methods';
+describe('Image Component', () => {
+  test('Image width and src', async () => {
 
-test('Image Comp', async () => {
+    const wrapper = mount(Image, {
+      propsData: {
+        width: 120,
+        src: "https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png",
+      },
+    });
+    await nextTick();
 
-  const wrapper = mount(Image, {
-    propsData: {
-      width: 120,
-      src: "https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png",
-    },
-  });
-  await nextTick();
+    const view = wrapper.find('.ste-image-root');
 
-  const view = wrapper.find('.ste-image-root');
-  const style = style2obj(view)
-  expect(style["--image-root-width"]).toEqual("60px");
-  const img = wrapper.find('.content');
-  const imgStyle = style2obj(img)
-  expect(imgStyle["opacity"]).toEqual("1");
+    const style = style2obj(view)
+    expect(style["--image-root-width"]).toEqual("60px");
+    const img = wrapper.find('.content');
+    const imgStyle = style2obj(img)
+    expect(imgStyle["opacity"]).toEqual("1");
+    expect(img.attributes().src).toEqual("https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png")
+    expect(img.attributes().mode).toEqual("scaleToFill")
+  })
+
+  test('Image radius', async () => {
+    const wrapper = mount(Image, {
+      propsData: {
+        width: 120,
+        src: "https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png",
+        radius: 10
+      },
+    })
+    await nextTick();
+    const view = wrapper.find('.ste-image-root');
+    const style = style2obj(view)
+    expect(style["--image-root-radius"]).toEqual("5px");
+  })
+
 })
