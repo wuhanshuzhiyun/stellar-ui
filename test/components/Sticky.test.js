92726832ad1f9968bd456a5ff74454b68d1eb9a1
徐亚军 <850674935@qq.com>
2025-02-10 11:46:14 +0800

    feat(Sticky): 测试

--- test/components/Sticky.test.js
+++ test/components/Sticky.test.js
@@ -1,33 +1,37 @@
-import { expect, test, vi } from 'vitest';
+import { expect, test, vi, describe } from 'vitest';
 import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import Sticky from '../../uni_modules/stellar-ui/components/ste-sticky/ste-sticky.vue';
 import { style2obj } from '../methods';
+describe('Sticky Comp', () => {
+  test('Sticky offsetTop', async () => {
+    const wrapper = mount(Sticky, {
+      propsData: {
+        offsetTop: 120,
+      },
+    });
+    await nextTick();
 
-test('Sticky Comp', async () => {
-  const wrapper = mount(Sticky, {
-    propsData: {
-      offsetTop: 120,
-    },
-  });
-  await nextTick();
-
-  const view = wrapper.find('.ste-sticky');
-  const style = style2obj(view)
-  expect(style.top).toBe('60px')
-  expect(style.position).toBe('sticky')
-  expect(wrapper.props().zIndex).toBe(98)
+    const view = wrapper.find('.ste-sticky');
+    const style = style2obj(view)
+    expect(style.top).toBe('60px')
+    expect(style.position).toBe('sticky')
+    expect(wrapper.props().zIndex).toBe(98)
 
 
-  const wrapper2 = mount(Sticky, {
-    propsData: {
-      disabled: true,
-      zIndex: 999,
-    },
+
   })
-  await nextTick();
+  test('Sticky disabled', async () => {
+    const wrapper = mount(Sticky, {
+      propsData: {
+        disabled: true,
+        zIndex: 999,
+      },
+    })
+    await nextTick();
 
-  const view2 = wrapper2.find('.ste-sticky');
-  const style2 = style2obj(view2)
-  expect(style2.position).toBeUndefined();
+    const view = wrapper.find('.ste-sticky');
+    const style = style2obj(view)
+    expect(style.position).toBeUndefined();
+  })
 })
