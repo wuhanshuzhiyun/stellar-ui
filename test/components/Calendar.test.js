b190b563c7bb395d0bacf372d1df968957b5948c
徐亚军 <850674935@qq.com>
2025-02-10 14:20:51 +0800

    feat(Calendar): 测试

---
+++ test/components/Calendar.test.js
@@ -0,0 +1,30 @@
+import { expect, test, vi, describe } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import Calendar from '../../uni_modules/stellar-ui/components/ste-calendar/ste-calendar.vue';
+
+describe('Calendar Component', () => {
+  test('Calendar title', async () => {
+    const wrapper = mount(Calendar, {
+      propsData: {
+        title: "选择日期",
+      },
+    });
+    await nextTick();
+
+    const title = wrapper.find('.calendar-title');
+    expect(title.text()).toBe("选择日期");
+  })
+
+  test('Calendar showTitle', async () => {
+    const wrapper = mount(Calendar, {
+      propsData: {
+        showTitle: false,
+      },
+    })
+    await nextTick();
+
+    const title = wrapper.find('.calendar-title');
+    expect(title.exists()).toBe(false);
+  })
+})
