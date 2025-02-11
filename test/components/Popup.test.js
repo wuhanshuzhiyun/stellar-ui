9e19738928d2a040dfdbd333b39cc6ec5d2d5add
付宇威 <f6512098@163.com>
2025-02-11 10:25:59 +0800

    测试用例

---
+++ test/components/Popup.test.js
@@ -0,0 +1,8 @@
+import { expect, test, vi, describe } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import stePopup from '../../uni_modules/stellar-ui/components/ste-popup/ste-popup.vue';
+
+describe('Test Popup', () => {
+	test('popup', () => {});
+});
