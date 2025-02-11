b17d7eb8884349b21a6e168c8035f7b1fdb77f8a
徐亚军 <850674935@qq.com>
2025-02-11 16:42:08 +0800

    feat(save):  优化测试代码

--- test/utils/Color.test.js
+++ test/utils/Color.test.js
@@ -1,15 +1,26 @@
-import { expect, test } from 'vitest';
 import Color from "../../uni_modules/stellar-ui/utils/Color.js"
 
-test("Color", async () => {
+test("hex2rgba", () => {
   const rgba1 = Color.hex2rgba("#ff0000")
   expect(rgba1).toBe("rgba(255,0,0,1)")
+})
+
+test("rgba2hex", () => {
   const rgba2 = Color.hexa2rgba("#f008")
   expect(rgba2).toBe("rgba(255,0,0,0.53)")
+})
+
+test("rgb2rgba", () => {
   const rgba3 = Color.rgb2rgba("rgb(255,0,0)", 0.5)
   expect(rgba3).toBe("rgba(255,0,0,0.5)")
+})
+
+test("setRgbaDiaphaneity", () => {
   const rgba4 = Color.setRgbaDiaphaneity("rgba(255,0,0,0.5)", 0.6)
   expect(rgba4).toBe("rgba(255,0,0,0.6)")
+})
+
+test("formatColor", async () => {
   const rgba5 = Color.formatColor("rgb(255,0,0)")
   expect(rgba5).toBe("rgba(255,0,0,1)")
 })
