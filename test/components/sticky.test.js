d7ad8a9002ef21889c458b40105b5a4b74709493
徐亚军 <850674935@qq.com>
2025-02-11 10:35:20 +0800

    feat(save):save

---
+++ test/components/sticky.test.js
@@ -0,0 +1,34 @@
+import { expect, test, vi, describe } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import Sticky from '../../uni_modules/stellar-ui/components/ste-sticky/ste-sticky.vue';
+import { style2obj } from '../methods';
+describe('Sticky Comp', () => {
+	test('Sticky offsetTop', async () => {
+		const wrapper = mount(Sticky, {
+			propsData: {
+				offsetTop: 120,
+			},
+		});
+		await nextTick();
+
+		const view = wrapper.find('.ste-sticky');
+		const style = style2obj(view);
+		expect(style.top).toBe('60px');
+		expect(style.position).toBe('sticky');
+		expect(wrapper.props().zIndex).toBe(98);
+	});
+	test('Sticky disabled', async () => {
+		const wrapper = mount(Sticky, {
+			propsData: {
+				disabled: true,
+				zIndex: 999,
+			},
+		});
+		await nextTick();
+
+		const view = wrapper.find('.ste-sticky');
+		const style = style2obj(view);
+		expect(style.position).toBeUndefined();
+	});
+});
