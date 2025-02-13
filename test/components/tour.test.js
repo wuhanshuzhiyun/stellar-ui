fd4f8c7fcaf9994f480bf71a0fc53d3f47bba09b
徐亚军 <850674935@qq.com>
2025-02-13 14:05:36 +0800

    feat(save): save

--- test/components/tour.test.js
+++ test/components/tour.test.js
@@ -2,37 +2,47 @@ import { nextTick } from 'vue';
 import Tour from '../../uni_modules/stellar-ui/components/ste-tour/ste-tour.vue';
 import { createVue } from '../methods';
 
-describe('Tour Component', () => {
-	test('Tour show', async () => {
-		const vm = createVue({
-			components: { "ste-tour": Tour },
-			template: `<div>
-				<div id="test-button" class="button-box">
-					<button>基础提示</button>
-				</div>
-				<ste-tour :show.sync="show" :steps="steps"></ste-tour>
-			</div>`,
-			data() {
-				return {
-					show: false,
-					steps: [{ message: '点这里', target: 'test-button' }],
-				}
+describe('Tour Component', async () => {
+	const vm = createVue({
+		components: { "ste-tour": Tour },
+		template: `<div>
+			<div id="test-button" class="button-box">
+				<button>基础提示</button>
+			</div>
+			<ste-tour :show.sync="show" :steps="steps"></ste-tour>
+		</div>`,
+		data() {
+			return {
+				show: false,
+				steps: [{ message: '点这里', target: 'test-button' }],
 			}
-		}, true)
-		await nextTick();
+		}
+	}, true)
+
+	await nextTick();
+
+	test("Tour init", async () => {
 		// 默认隐藏
-		const el1 = vm.$el.querySelector('[data-test="tour"]')
-		expect(el1.classList.toString().indexOf('show') === -1).toBe(true);
+		const el = vm.$el.querySelector('[data-test="tour"]')
+		expect(el.classList.toString().indexOf('show') === -1).toBe(true);
+	})
+
+	test('Tour show', async () => {
 		// 点击显示
 		vm.show = true;
 		await nextTick();
 		const el = vm.$el.querySelector('[data-test="tour"]')
 		expect(el.classList.toString().indexOf('show') !== -1).toBe(true);
 
+
+	});
+
+	test('Tour hide', async () => {
 		// 点击隐藏
 		vm.show = false;
 		await nextTick();
+		const el = vm.$el.querySelector('[data-test="tour"]')
 		expect(el.classList.toString().indexOf('show') === -1).toBe(true);
-	});
+	})
 
 });
