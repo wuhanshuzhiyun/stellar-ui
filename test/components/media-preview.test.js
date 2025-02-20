d34ae3f5137df77bd6e1251e00ea844bb76204fe
徐亚军 <850674935@qq.com>
2025-02-20 15:44:25 +0800

    feat(save): save

--- test/components/media-preview.test.js
+++ test/components/media-preview.test.js
@@ -1,41 +1,46 @@
 import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import MediaPreview from '../../uni_modules/stellar-ui/components/ste-media-preview/ste-media-preview.vue';
-import { style2obj } from '../methods';
 import { expect } from 'vitest';
 
-describe('MediaPreview Component', () => {
-	test('MediaPreview default', async () => {
-		const wrapper = mount(MediaPreview, {
-			propsData: {
-			},
-		});
-		await nextTick();
-
-		const upload = wrapper.find('[data-test="media-preview"]');
-		expect(upload.exists()).toBe(false);
+describe('MediaPreview Component', async () => {
+	const wrapper = mount(MediaPreview, {
+		propsData: {
+			show: true,
+			urls: ["https://www.baidu.com/img/flexible/logo/pc/result.png", "https://www.baidu.com/img/flexible/logo/pc/result.png"],
+			autoplay: 0,
+			loop: false,
+			index: null,
+			showIndex: true,
+			scale: false,
+			showmenu: true,
+		},
 	});
-	test('MediaPreview show', async () => {
-		const wrapper = mount(MediaPreview, {
-			propsData: {
-				show: true,
-			},
-		});
-		await nextTick();
-
+	await nextTick();
+	test('show', async () => {
 		const upload = wrapper.find('[data-test="media-preview"]');
 		expect(upload.exists()).toBe(true);
 	});
-	test('MediaPreview urls', async () => {
-		const wrapper = mount(MediaPreview, {
-			propsData: {
-				show: true,
-				urls: ["https://www.baidu.com/img/flexible/logo/pc/result.png", "https://www.baidu.com/img/flexible/logo/pc/result.png"],
-			},
-		});
-		await nextTick();
-
+	test('urls', async () => {
 		const upload = wrapper.findAll('[data-test="media-preview-item"]');
 		expect(upload.length).toBe(2);
 	});
+	test('autoplay', async () => {
+		expect(wrapper.props('autoplay')).toBe(0);
+	})
+	test('loop', async () => {
+		expect(wrapper.props('loop')).toBe(false);
+	})
+	test('index', async () => {
+		expect(wrapper.props('index')).toBe(null);
+	})
+	test('showIndex', async () => {
+		expect(wrapper.props('showIndex')).toBe(true);
+	})
+	test('scale', async () => {
+		expect(wrapper.props('scale')).toBe(false);
+	})
+	test('showmenu', async () => {
+		expect(wrapper.props('showmenu')).toBe(true);
+	})
 });
