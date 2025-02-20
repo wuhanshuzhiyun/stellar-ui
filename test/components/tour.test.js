cfd592d4433f4999e562374a5c9051492b6addc1
徐亚军 <850674935@qq.com>
2025-02-20 16:23:52 +0800

    feat(save): save

--- test/components/tour.test.js
+++ test/components/tour.test.js
@@ -1,8 +1,9 @@
+import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import Tour from '../../uni_modules/stellar-ui/components/ste-tour/ste-tour.vue';
 import { createVue, triggerClick } from '../methods';
 
-describe('Tour Component', async () => {
+describe('Tour Vue Component', async () => {
 	const vm = createVue({
 		components: { "ste-tour": Tour },
 		template: `<div>
@@ -22,15 +23,15 @@ describe('Tour Component', async () => {
 		}
 	}, true)
 
-	await nextTick();
+	await new Promise(p => setTimeout(p, 500));
 
-	test("Tour init", async () => {
+	test("init", async () => {
 		// 默认隐藏
 		const el = vm.$el.querySelector('[data-test="tour"]')
 		expect(el.classList.toString().indexOf('show') === -1).toBe(true);
 	})
 
-	test('Tour show', async () => {
+	test('show', async () => {
 		// 点击显示
 		vm.show = true;
 		await nextTick();
@@ -40,7 +41,7 @@ describe('Tour Component', async () => {
 
 	});
 
-	test('Tour hide', async () => {
+	test('hide', async () => {
 		// 点击隐藏
 		vm.show = false;
 		await nextTick();
@@ -48,7 +49,7 @@ describe('Tour Component', async () => {
 		expect(el.classList.toString().indexOf('show') === -1).toBe(true);
 	})
 
-	test("Tour steps", async () => {
+	test("steps", async () => {
 		const vm = createVue({
 			components: { "ste-tour": Tour },
 			template: `<div>
@@ -84,4 +85,63 @@ describe('Tour Component', async () => {
 		expect(next1.innerHTML.trim()).toBe("完成")
 	})
 
-});
+	const wrapper = mount(Upload, {
+		propsData: {
+			show: true,
+			steps: [{ message: '点这里', target: 'test-button1' }],
+			offset: [0, 0],
+			showTitleBar: false,
+			showFooter: true,
+			mask: true,
+			maskColse: true,
+			showPrevStep: true,
+			background: 'rgba(0,0,0,.5)',
+			radius: 18,
+			messageBg: "#ffffff",
+			messageColor: "#000000",
+			nextStepTxt: "下一步",
+			prevStepTxt: "上一步",
+			completeTxt: "完成",
+		}
+	})
+	// await nextTick()
+	// test("offset", () => {
+	// 	expect(wrapper.props("offset")).toEqual([0, 0]);
+	// })
+	// test("showTitleBar", () => {
+	// 	expect(wrapper.props("showTitleBar")).toBe(false);
+	// })
+	// test("showFooter", () => {
+	// 	expect(wrapper.props("showFooter")).toBe(true);
+	// })
+	// test("mask", () => {
+	// 	expect(wrapper.props("mask")).toBe(true);
+	// })
+	// test("maskColse", () => {
+	// 	expect(wrapper.props("maskColse")).toBe(true);
+	// })
+	// test("showPrevStep", () => {
+	// 	expect(wrapper.props("showPrevStep")).toBe(true);
+	// })
+	// test("background", () => {
+	// 	expect(wrapper.props("background")).toBe("rgba(0,0,0,.5)");
+	// })
+	// test("radius", () => {
+	// 	expect(wrapper.props("radius")).toBe(18);
+	// })
+	// test("messageBg", () => {
+	// 	expect(wrapper.props("messageBg")).toBe("#ffffff");
+	// })
+	// test("messageColor", () => {
+	// 	expect(wrapper.props("messageColor")).toBe("#000000");
+	// })
+	// test("nextStepTxt", () => {
+	// 	expect(wrapper.props("nextStepTxt")).toBe("下一步");
+	// })
+	// test("prevStepTxt", () => {
+	// 	expect(wrapper.props("prevStepTxt")).toBe("上一步");
+	// })
+	// test("completeTxt", () => {
+	// 	expect(wrapper.props("completeTxt")).toBe("完成");
+	// })
+})
