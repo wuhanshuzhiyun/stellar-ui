144e6873a8d2662b238f0e0c3d974a9448716686
徐亚军 <850674935@qq.com>
2025-02-20 11:23:48 +0800

    feat(save): save

--- test/components/signature.test.js
+++ test/components/signature.test.js
@@ -3,21 +3,48 @@ import { nextTick } from 'vue';
 import Signature from '../../uni_modules/stellar-ui/components/ste-signature/ste-signature.vue';
 import { style2obj } from '../methods';
 
-describe('Signature Component', () => {
-	test('Signature size and class', async () => {
-		const wrapper = mount(Signature, {
-			propsData: {
-				customClass: 'test-class',
-				width: 100,
-				height: 100,
-			},
-		});
-		await nextTick();
+describe('Signature Component', async () => {
+	const wrapper = mount(Signature, {
+		propsData: {
+			customClass: 'test-class',
+			lineWidth: 10,
+			strokeColor: "#000000",
+			background: "#ffffff",
+			type: "png",
+			width: 100,
+			height: 100,
 
-		const nav = wrapper.find('.test-class');
-		const style = style2obj(nav)
-		expect(nav.exists()).toBe(true);
-		expect(style.width).toBe('50px');
-		expect(style.height).toBe('50px');
+		},
 	});
+	await nextTick();
+
+	test("customClass", async () => {
+		expect(wrapper.classes()).toContain('test-class');
+	})
+
+	test("lineWidth", async () => {
+		expect(wrapper.props().lineWidth).toBe(10);
+	})
+
+	test("strokeColor", async () => {
+		expect(wrapper.props().strokeColor).toBe("#000000");
+	})
+
+	test("background", async () => {
+		expect(wrapper.props().background).toBe("#ffffff");
+	})
+
+	test("type", async () => {
+		expect(wrapper.props().type).toBe("png");
+	})
+
+	const el = wrapper.find('.test-class');
+	const style = style2obj(el)
+
+	test('width', async () => {
+		expect(style.width).toBe('50px');
+	});
+	test('height', async () => {
+		expect(style.height).toBe('50px');
+	})
 });
