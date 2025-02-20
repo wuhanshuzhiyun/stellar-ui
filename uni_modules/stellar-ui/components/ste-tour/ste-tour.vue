dd307c27368787787d28433139476dedca2c7046
徐亚军 <850674935@qq.com>
2025-02-20 16:24:52 +0800

    feat(fix): 修复兼容性

--- uni_modules/stellar-ui/components/ste-tour/ste-tour.vue
+++ uni_modules/stellar-ui/components/ste-tour/ste-tour.vue
@@ -1,15 +1,10 @@
 <template>
-	<view
-		class="ste-tour-root"
-		@touchmove.stop="true"
-		:class="{ show: dataShow }"
-		:style="[cmpRootStyle]"
-		@click="clickRoot"
-	>
+	<view class="ste-tour-root" @touchmove.stop="true" :class="{ show: dataShow }" :style="[cmpRootStyle]"
+		@click="clickRoot">
 		<view class="ste-tour-view" :style="[dataStyle]"></view>
 		<view class="ste-tour-message" :style="[messageStyle]">
 			<view class="message-arrows" :style="[arrowsStyle]" />
-			<view class="message-content">
+			<view class="message-content" v-if="cmpStep">
 				<view class="message-head" v-if="showTitleBar" @click.stop="true">
 					<view class="head-title">{{ cmpStep.title }}</view>
 					<view class="head-close" @click="close">
@@ -26,22 +21,12 @@
 				<view class="message-step-footer" @click.stop="true" v-if="cmpShowFooter">
 					<view class="step-num">{{ dataCurrent + 1 }}/{{ steps.length }}</view>
 					<view class="step-btns">
-						<ste-button
-							:round="false"
-							background="#fff"
-							borderColor="#0090FF"
-							color="#0090FF"
-							style="margin-right: 12rpx"
-							mode="100"
-							@click="onUp"
-							v-if="cmpShowPrevStep"
-							:rootStyle="{ padding: '0 10px' }"
-						>
+						<ste-button :round="false" background="#fff" borderColor="#0090FF" color="#0090FF" style="margin-right: 12rpx"
+							mode="100" @click="onUp" v-if="cmpShowPrevStep" :rootStyle="{ padding: '0 10px' }">
 							{{ prevStepTxt }}
 						</ste-button>
 						<ste-button :round="false" :rootStyle="{ padding: '0 10px' }" mode="100" @click="onNext">
-							{{ dataCurrent < steps.length - 1 ? nextStepTxt : completeTxt }}
-						</ste-button>
+							{{ dataCurrent < steps.length - 1 ? nextStepTxt : completeTxt }} </ste-button>
 					</view>
 				</view>
 			</view>
@@ -282,9 +267,11 @@ export default {
 	left: 0;
 	overflow: hidden;
 	display: none;
+
 	&.show {
 		display: block;
 	}
+
 	.ste-tour-view {
 		border-radius: var(--ste-tour-radius);
 		position: fixed;
@@ -292,6 +279,7 @@ export default {
 		box-shadow: var(--ste-tour-mask);
 		display: none;
 	}
+
 	.ste-tour-message {
 		background-color: var(--ste-tour-message-bg);
 		position: fixed;
@@ -299,6 +287,7 @@ export default {
 		border-radius: var(--ste-tour-radius);
 		display: none;
 		box-shadow: var(--ste-tour-message-shadow);
+
 		.message-arrows {
 			position: absolute;
 			width: 0;
@@ -307,6 +296,7 @@ export default {
 			border-style: solid;
 			border-color: transparent transparent var(--ste-tour-message-bg);
 		}
+
 		.message-content {
 			color: var(--ste-tour-message-color);
 			font-size: 24rpx;
@@ -317,6 +307,7 @@ export default {
 				align-items: center;
 				min-width: 360rpx;
 				padding: 20rpx 24rpx;
+
 				.head-title {
 					height: 60rpx;
 					line-height: 60rpx;
@@ -331,6 +322,7 @@ export default {
 					line-height: 30rpx;
 				}
 			}
+
 			.message-content-text {
 				.message-text {
 					padding: 20rpx 24rpx;
@@ -343,6 +335,7 @@ export default {
 				align-items: center;
 				padding: 20rpx 24rpx;
 				min-width: 360rpx;
+
 				.step-num {
 					font-size: 24rpx;
 					color: var(--ste-tour-message-color);
