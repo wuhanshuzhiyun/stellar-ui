6d3ed1a6e83aa3995de82f6c1b242602c0298fe0
徐亚军 <850674935@qq.com>
2025-02-20 10:31:29 +0800

    feat(save): save

--- test/components/image.test.js
+++ test/components/image.test.js
@@ -2,38 +2,71 @@ import { mount } from '@vue/test-utils';
 import { nextTick } from 'vue';
 import Image from '../../uni_modules/stellar-ui/components/ste-image/ste-image.vue';
 import { style2obj } from '../methods';
-describe('Image Component', () => {
-	test('Image width and src', async () => {
-		const wrapper = mount(Image, {
-			propsData: {
-				width: 120,
-				src: 'https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png',
-			},
-		});
-		await nextTick();
+describe('Image Component', async () => {
+	const wrapper = mount(Image, {
+		propsData: {
+			src: 'https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png',
+			mode: "scaleToFill",
+			width: 120,
+			height: 120,
+			radius: 60,
+			hiddenLoading: true,
+			hiddenError: true,
+			showMenuByLongpress: true,
+			lazyLoad: true,
+			display: "inline-flex"
+		},
+	});
+	await nextTick();
 
-		const view = wrapper.find('.ste-image-root');
 
-		const style = style2obj(view);
-		expect(style['--image-root-width']).toEqual('60px');
+	test('mode', async () => {
+		const img = wrapper.find('.content');
+		expect(img.attributes().mode).toEqual('scaleToFill');
+	})
+
+	test('height', async () => {
 		const img = wrapper.find('.content');
 		const imgStyle = style2obj(img);
 		expect(imgStyle['opacity']).toEqual('1');
 		expect(img.attributes().src).toEqual('https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png');
-		expect(img.attributes().mode).toEqual('scaleToFill');
-	});
+	})
 
-	test('Image radius', async () => {
-		const wrapper = mount(Image, {
-			propsData: {
-				width: 120,
-				src: 'https://image.whzb.com/chain/StellarUI/%E6%94%AF%E4%BB%98%E5%AE%9D.png',
-				radius: 10,
-			},
-		});
-		await nextTick();
+	test('width', async () => {
 		const view = wrapper.find('.ste-image-root');
 		const style = style2obj(view);
-		expect(style['--image-root-radius']).toEqual('5px');
+		expect(style['--image-root-width']).toEqual('60px');
 	});
+
+	test('height', async () => {
+		const view = wrapper.find('.ste-image-root');
+		const style = style2obj(view);
+		expect(style['--image-root-height']).toEqual('60px');
+	})
+
+
+	test('radius', async () => {
+		const view = wrapper.find('.ste-image-root');
+		const style = style2obj(view);
+		expect(style['--image-root-radius']).toEqual('30px');
+	});
+	test('hiddenLoading', async () => {
+		expect(wrapper.props().hiddenLoading).toBe(true);
+	})
+
+	test('hiddenError', async () => {
+		expect(wrapper.props().hiddenError).toBe(true);
+	})
+	test('showMenuByLongpress', async () => {
+		expect(wrapper.props().showMenuByLongpress).toBe(true);
+	})
+	test('lazyLoad', async () => {
+		expect(wrapper.props().lazyLoad).toBe(true);
+	})
+	test('display', async () => {
+		const view = wrapper.find('.ste-image-root');
+		const style = style2obj(view);
+		expect(style['--image-root-display']).toEqual('inline-flex');
+	})
+
 });
