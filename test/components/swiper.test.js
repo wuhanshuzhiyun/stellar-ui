5385723842647f4231a89197e9a2ee081423153e
徐亚军 <850674935@qq.com>
2025-02-21 10:26:24 +0800

    feat(save): save

--- test/components/swiper.test.js
+++ test/components/swiper.test.js
@@ -2,6 +2,7 @@ import { nextTick } from 'vue';
 import Swiper from '../../uni_modules/stellar-ui/components/ste-swiper/ste-swiper.vue';
 import SwiperItem from '../../uni_modules/stellar-ui/components/ste-swiper-item/ste-swiper-item.vue';
 import { createVue } from '../methods';
+import { mount } from '@vue/test-utils';
 
 describe('Swiper Component', async () => {
 	const vm = createVue({
@@ -29,28 +30,44 @@ describe('Swiper Component', async () => {
 			}
 		}
 	}, true)
-	await nextTick();
 
-	test("Swiper list-item", async () => {
+	const wrapper = mount(Swiper, {
+		propsData: {
+			disabled: false,
+			width: 750,
+			height: 1334,
+			duration: 300,
+			swipeThreshold: 0.35,
+			indicatorDots: false,
+			indicatorColor: "#ffffff",
+			indicatorActiveColor: "#ffffff",
+			autoplay: false,
+			interval: 3000,
+			circular: false,
+			previousMargin: 0,
+			nextMargin: 0
+		}
+	})
+
+	await new Promise(r => setTimeout(r, 500));
+
+	test("list-item", async () => {
 		const list = vm.$el.querySelectorAll('[data-test="swiper-item"]');
 		expect(list.length).toBe(8);
 	})
 
-	test("Swiper position", async () => {
+	test("position", async () => {
 		const translateX = vm.$refs.swiper.translateX
 		expect(translateX).toBe(0)
-
-
-
 	})
 
-	test("Swiper current", async () => {
+	test("current", async () => {
 		vm.current = 1
 		await new Promise(resolve => setTimeout(resolve, 500))
 		expect(vm.$refs.swiper.translateX).toBe(-375)
 	})
 
-	test("Swiper vertical", async () => {
+	test("direction-vertical", async () => {
 		const vm = createVue({
 			components: { "ste-swiper": Swiper, "ste-swiper-item": SwiperItem },
 			template: `<ste-swiper ref="swiper" :current.sync="current" height="1334" direction="vertical">
@@ -81,5 +98,33 @@ describe('Swiper Component', async () => {
 		vm.current = 1
 		await new Promise(resolve => setTimeout(resolve, 500))
 		expect(vm.$refs.swiper.translateY).toBe(-667)
+	})
+
+	test("disabled", () => {
+		expect(wrapper.props().disabled).toBe(false)
+	})
+
+	test("autoplay", () => {
+		expect(wrapper.props().autoplay).toBe(false)
+	})
+
+	test("current", () => {
+		expect(wrapper.props().current).toBe(0)
+	})
+
+	test("interval", () => {
+		expect(wrapper.props().interval).toBe(3000)
+	})
+
+	test("circular", () => {
+		expect(wrapper.props().circular).toBe(false)
+	})
+
+	test("previousMargin", () => {
+		expect(wrapper.props().previousMargin).toBe(0)
+	})
+
+	test("nextMargin", () => {
+		expect(wrapper.props().nextMargin).toBe(0)
 	})
 });
