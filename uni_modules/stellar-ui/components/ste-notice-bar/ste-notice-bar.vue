1c8d3e32f93f56c644b63b43f0c3d82b101b6a77
徐亚军 <850674935@qq.com>
2025-11-26 14:36:06 +0800

    fix(save): 修复滚动属性scrollable在APP端失效的BUG

--- uni_modules/stellar-ui/components/ste-notice-bar/ste-notice-bar.vue
+++ uni_modules/stellar-ui/components/ste-notice-bar/ste-notice-bar.vue
@@ -11,7 +11,8 @@
 					<ste-rich-text :text="list[index]"></ste-rich-text>
 				</view>
 			</view>
-			<view v-else :class="'center vertical ' + cardMsgClass" @click="handleClick" @animationend="onAnimationEnd" :style="[cmpVerticalStyle]">
+			<view v-else :class="'center vertical ' + cardMsgClass" @click="handleClick" @animationend="onAnimationEnd"
+				:style="[cmpVerticalStyle]">
 				<view v-for="(item, i) in copyList" :key="i">
 					<ste-rich-text :text="item"></ste-rich-text>
 				</view>
@@ -28,303 +29,306 @@
 </template>
 
 <script>
-import steRichText from '../ste-rich-text/ste-rich-text.vue';
-import utils from '../../utils/utils.js';
+	import steRichText from '../ste-rich-text/ste-rich-text.vue';
+	import utils from '../../utils/utils.js';
 
-/**
- * ste-notice-bar 公告栏
- * @description 用于循环播放展示一组消息通知。
- * @tutorial https://stellar-ui.intecloud.com.cn/?projectName=stellar-ui&menu=%E7%BB%84%E4%BB%B6&active=ste-notice-bar
- * @property {Array} list 滚动数据列表，默认 []
- * @property {String} direction 滚动的方向，默认 across
- * @value across 水平 {String}
- * @value vertical 垂直 {String}
- * @property {Boolean} closeMode 是否启用关闭模式 默认 false
- * @property {String} color 文字颜色，默认 #000000
- * @property {String} background 背景，默认 #ffffff
- * @property {Number|String} width 宽度  默认 100%
- * @value 100% 填满 {String}
- * @value  {{Number}} 自定义宽度，单位rpx {Number}
- * @property {Number} acrossSpeed，滚动速率 (px/s)，默认 50
- * @property {Number} verticalSpeed，滚动的速度（ms），默认 500
- * @property {Number} delay 延时（ms），默认 1000
- * @property {Number} standTime 每次滚动前停留多少毫秒(竖向滚动时有效)（ms），默认 1000
- * @property {Boolean} scrollable 是否可以滚动 默认 true
- * @event {Function} click 外层点击事件回调
- * @event {Function} close 关闭通知栏时触发
- * @event {Function} end 滚动结束时触发
- */
+	/**
+	 * ste-notice-bar 公告栏
+	 * @description 用于循环播放展示一组消息通知。
+	 * @tutorial https://stellar-ui.intecloud.com.cn/?projectName=stellar-ui&menu=%E7%BB%84%E4%BB%B6&active=ste-notice-bar
+	 * @property {Array} list 滚动数据列表，默认 []
+	 * @property {String} direction 滚动的方向，默认 across
+	 * @value across 水平 {String}
+	 * @value vertical 垂直 {String}
+	 * @property {Boolean} closeMode 是否启用关闭模式 默认 false
+	 * @property {String} color 文字颜色，默认 #000000
+	 * @property {String} background 背景，默认 #ffffff
+	 * @property {Number|String} width 宽度  默认 100%
+	 * @value 100% 填满 {String}
+	 * @value  {{Number}} 自定义宽度，单位rpx {Number}
+	 * @property {Number} acrossSpeed，滚动速率 (px/s)，默认 50
+	 * @property {Number} verticalSpeed，滚动的速度（ms），默认 500
+	 * @property {Number} delay 延时（ms），默认 1000
+	 * @property {Number} standTime 每次滚动前停留多少毫秒(竖向滚动时有效)（ms），默认 1000
+	 * @property {Boolean} scrollable 是否可以滚动 默认 true
+	 * @event {Function} click 外层点击事件回调
+	 * @event {Function} close 关闭通知栏时触发
+	 * @event {Function} end 滚动结束时触发
+	 */
 
-export default {
-	group: '展示组件',
-	title: 'NoticeBar 公告栏',
-	name: 'ste-notice-bar',
-	options: {
-		virtualHost: true,
-	},
-	components: {
-		steRichText,
-	},
-	props: {
-		list: {
-			type: [Array, null],
-			default: () => [],
+	export default {
+		group: '展示组件',
+		title: 'NoticeBar 公告栏',
+		name: 'ste-notice-bar',
+		options: {
+			virtualHost: true,
 		},
-		direction: {
-			type: [String, null],
-			default: 'across',
+		components: {
+			steRichText,
 		},
-		closeMode: {
-			type: [Boolean, null],
-			default: false,
+		props: {
+			list: {
+				type: [Array, null],
+				default: () => [],
+			},
+			direction: {
+				type: [String, null],
+				default: 'across',
+			},
+			closeMode: {
+				type: [Boolean, null],
+				default: false,
+			},
+			color: {
+				type: [String, null],
+				default: '#000000',
+			},
+			background: {
+				type: [String, null],
+				default: '#ffffff',
+			},
+			width: {
+				type: [Number, String, null],
+				default: '100%',
+			},
+			acrossSpeed: {
+				type: [Number, null],
+				default: 50,
+			},
+			verticalSpeed: {
+				type: [Number, null],
+				default: 500,
+			},
+			delay: {
+				type: [Number, null],
+				default: 1000,
+			},
+			standTime: {
+				type: [Number, null],
+				default: 1000,
+			},
+			scrollable: {
+				type: [Boolean, null],
+				default: true,
+			},
 		},
-		color: {
-			type: [String, null],
-			default: '#000000',
+		data() {
+			return {
+				id: utils.guid(),
+				index: 0,
+				touch: false,
+				firstDone: false,
+				copyList: [],
+				acrossDuration: 0,
+				textTranslate: 0,
+				cardMsgClass: '',
+				h5flag: false,
+				closeShow: true,
+			};
 		},
-		background: {
-			type: [String, null],
-			default: '#ffffff',
+		async mounted() {},
+		watch: {
+			list: {
+				handler(val) {
+					this.copyList = utils.deepClone(val);
+					if (val.length) {
+						this.$nextTick(() => {
+							this.handleAnimation();
+						});
+					}
+				},
+				deep: true,
+				immediate: true,
+			},
+			index: {
+				handler() {
+					this.h5flag = false;
+				},
+				immediate: true,
+			},
+			copyList: {
+				handler() {
+					this.h5flag = false;
+				},
+				immediate: true,
+			},
 		},
-		width: {
-			type: [Number, String, null],
-			default: '100%',
+		computed: {
+			cmpStyle() {
+				let style = {};
+				style['width'] = isNaN(Number(this.width)) ? this.width : utils.formatPx(this.width);
+				style['background'] = this.background;
+				style['color'] = this.color;
+				return style;
+			},
+			cmpAcrossStyle() {
+				let style = {};
+				style['animationPlayState'] = this.touch ? 'paused' : 'running';
+				style['animationDuration'] = this.acrossDuration + 'ms';
+				style['animationDelay'] = (this.firstDone ? 0 : this.delay) + 'ms';
+				style['transform'] = `translateX(${this.textTranslate}px)`;
+				return style;
+			},
+			cmpVerticalStyle() {
+				let style = {};
+				style['animationPlayState'] = this.touch ? 'paused' : 'running';
+				style['animationDuration'] = this.verticalSpeed + 'ms';
+				style['animationDelay'] = (this.firstDone ? 0 : this.delay) + 'ms';
+				return style;
+			},
 		},
-		acrossSpeed: {
-			type: [Number, null],
-			default: 50,
-		},
-		verticalSpeed: {
-			type: [Number, null],
-			default: 500,
-		},
-		delay: {
-			type: [Number, null],
-			default: 1000,
-		},
-		standTime: {
-			type: [Number, null],
-			default: 1000,
-		},
-		scrollable: {
-			type: [Boolean, null],
-			default: true,
-		},
-	},
-	data() {
-		return {
-			id: utils.guid(),
-			index: 0,
-			touch: false,
-			firstDone: false,
-			copyList: [],
-			acrossDuration: 0,
-			textTranslate: 0,
-			cardMsgClass: '',
-			h5flag: false,
-			closeShow: true,
-		};
-	},
-	async mounted() {},
-	watch: {
-		list: {
-			handler(val) {
-				this.copyList = utils.deepClone(val);
-				if (val.length && this.scrollable) {
-					this.$nextTick(() => {
-						this.handleAnimation();
-					});
+		methods: {
+			// 执行滚动动画
+			async handleAnimation() {
+				if (!this.scrollable) return
+				if (this.direction == 'across') {
+					// 获取滚动消息的长度来计算动画的执行时间
+					let dom = await utils.querySelector('#' + this.id, this);
+					this.acrossDuration = ((dom.width + this.textTranslate) / Number(this.acrossSpeed)) * 1000;
+					this.cardMsgClass = 'across-play-infinite';
+				} else {
+					this.cardMsgClass = 'vertical-play-infinite';
 				}
 			},
-			deep: true,
-			immediate: true,
-		},
-		index: {
-			handler() {
-				this.h5flag = false;
-			},
-			immediate: true,
-		},
-		copyList: {
-			handler() {
-				this.h5flag = false;
-			},
-			immediate: true,
-		},
-	},
-	computed: {
-		cmpStyle() {
-			let style = {};
-			style['width'] = isNaN(Number(this.width)) ? this.width : utils.formatPx(this.width);
-			style['background'] = this.background;
-			style['color'] = this.color;
-			return style;
-		},
-		cmpAcrossStyle() {
-			let style = {};
-			style['animationPlayState'] = this.touch ? 'paused' : 'running';
-			style['animationDuration'] = this.acrossDuration + 'ms';
-			style['animationDelay'] = (this.firstDone ? 0 : this.delay) + 'ms';
-			style['transform'] = `translateX(${this.textTranslate}px)`;
-			return style;
-		},
-		cmpVerticalStyle() {
-			let style = {};
-			style['animationPlayState'] = this.touch ? 'paused' : 'running';
-			style['animationDuration'] = this.verticalSpeed + 'ms';
-			style['animationDelay'] = (this.firstDone ? 0 : this.delay) + 'ms';
-			return style;
-		},
-	},
-	methods: {
-		// 执行滚动动画
-		async handleAnimation() {
-			if (this.direction == 'across') {
-				// 获取滚动消息的长度来计算动画的执行时间
-				let dom = await utils.querySelector('#' + this.id, this);
-				this.acrossDuration = ((dom.width + this.textTranslate) / Number(this.acrossSpeed)) * 1000;
-				this.cardMsgClass = 'across-play-infinite';
-			} else {
-				this.cardMsgClass = 'vertical-play-infinite';
-			}
-		},
-		// 动画结束
-		async onAnimationEnd() {
-			// 解决h5动画初始会触发结束事件
-			// #ifdef H5 || APP
-			if (this.h5flag) {
-				// #endif
-				if (this.direction == 'across') {
-					if (!this.textTranslate) {
-						// 获取滚动消息的父级元素的长度实现无缝滚动
-						let dom = await utils.querySelector('.' + this.id, this);
-						this.textTranslate = dom.width;
+			// 动画结束
+			async onAnimationEnd() {
+				// 解决h5动画初始会触发结束事件
+				// #ifdef H5 || APP
+				if (this.h5flag) {
+					// #endif
+					if (this.direction == 'across') {
+						if (!this.textTranslate) {
+							// 获取滚动消息的父级元素的长度实现无缝滚动
+							let dom = await utils.querySelector('.' + this.id, this);
+							this.textTranslate = dom.width;
+						}
+						this.cardMsgClass = '';
+						// h5里 文本内容切换会导致触发动画结束事件，屏蔽切换
+						this.index = this.index + 1 >= this.list.length ? 0 : this.index + 1;
+						this.$nextTick(() => {
+							this.handleAnimation();
+						});
+					} else {
+						this.cardMsgClass = '';
+						// h5里 文本内容切换会导致触发动画结束事件，屏蔽切换
+
+						this.copyList.push(this.copyList.shift());
+
+						this.index = this.index + 1 >= this.list.length ? 0 : this.index + 1;
+						setTimeout(
+							() => {
+								this.handleAnimation();
+							},
+							this.standTime > this.verticalSpeed ? this.standTime : this.verticalSpeed // 下一个动画要在前一个动画后
+						);
 					}
-					this.cardMsgClass = '';
-					// h5里 文本内容切换会导致触发动画结束事件，屏蔽切换
-					this.index = this.index + 1 >= this.list.length ? 0 : this.index + 1;
-					this.$nextTick(() => {
-						this.handleAnimation();
-					});
+					this.firstDone = true;
+					this.$emit('end', this.index);
+					// #ifdef H5 || APP
+				}
+				if (this.direction == 'across') {
+					this.h5flag = true;
 				} else {
-					this.cardMsgClass = '';
-					// h5里 文本内容切换会导致触发动画结束事件，屏蔽切换
-
-					this.copyList.push(this.copyList.shift());
-
-					this.index = this.index + 1 >= this.list.length ? 0 : this.index + 1;
 					setTimeout(
 						() => {
-							this.handleAnimation();
+							this.h5flag = true;
 						},
-						this.standTime > this.verticalSpeed ? this.standTime : this.verticalSpeed // 下一个动画要在前一个动画后
+						this.verticalSpeed > 20 ? this.verticalSpeed - 20 : 20
 					);
 				}
-				this.firstDone = true;
-				this.$emit('end', this.index);
-				// #ifdef H5 || APP
-			}
-			if (this.direction == 'across') {
-				this.h5flag = true;
-			} else {
-				setTimeout(
-					() => {
-						this.h5flag = true;
-					},
-					this.verticalSpeed > 20 ? this.verticalSpeed - 20 : 20
-				);
-			}
-			// #endif
+				// #endif
+			},
+			// 暂停
+			doPause() {
+				this.touch = true;
+			},
+			// 取消暂停
+			doRun() {
+				this.touch = false;
+			},
+			handleClose(e) {
+				this.closeShow = false;
+				this.$emit('close', e);
+			},
+			handleClick(e) {
+				this.$emit('click', this.index);
+			},
 		},
-		// 暂停
-		doPause() {
-			this.touch = true;
-		},
-		// 取消暂停
-		doRun() {
-			this.touch = false;
-		},
-		handleClose(e) {
-			this.closeShow = false;
-			this.$emit('close', e);
-		},
-		handleClick(e) {
-			this.$emit('click', this.index);
-		},
-	},
-};
+	};
 </script>
 
 <style lang="scss" scoped>
-.ste-notice-bar-root {
-	width: 100%;
-	height: 68rpx;
-	padding: 16rpx;
-	background: #ffffff;
-	border-radius: 16rpx;
-	display: flex;
-	align-items: center;
-
-	.msg-box-content {
+	.ste-notice-bar-root {
+		width: 100%;
+		height: 68rpx;
+		padding: 16rpx;
+		background: #ffffff;
+		border-radius: 16rpx;
 		display: flex;
 		align-items: center;
-		overflow-x: hidden;
-		overflow-y: hidden;
-		justify-content: flex-start;
-		flex: 1;
-		height: 100%;
-	}
-	.left {
-		flex-shrink: 0;
-		margin-right: 16rpx;
-		display: flex;
-		align-items: center;
-		height: 36rpx;
-		line-height: 36rpx;
-	}
-	.center {
-		flex: 1;
-		display: flex;
-		overflow: hidden;
 
-		> view {
-			font-size: 24rpx;
-			display: inline-block;
-			width: auto;
-			white-space: nowrap;
+		.msg-box-content {
+			display: flex;
+			align-items: center;
+			overflow-x: hidden;
+			overflow-y: hidden;
+			justify-content: flex-start;
+			flex: 1;
+			height: 100%;
 		}
 
-		.across-play-infinite {
-			animation: acrossAnimation linear both running;
+		.left {
+			flex-shrink: 0;
+			margin-right: 16rpx;
+			display: flex;
+			align-items: center;
+			height: 36rpx;
+			line-height: 36rpx;
+		}
+
+		.center {
+			flex: 1;
+			display: flex;
+			overflow: hidden;
+
+			>view {
+				font-size: 24rpx;
+				display: inline-block;
+				width: auto;
+				white-space: nowrap;
+			}
+
+			.across-play-infinite {
+				animation: acrossAnimation linear both running;
+				animation-iteration-count: 1;
+			}
+
+			@keyframes acrossAnimation {
+				100% {
+					transform: translateX(-100%);
+				}
+			}
+		}
+
+		.vertical {
+			flex-direction: column;
+			height: 36rpx;
+			line-height: 36rpx;
+		}
+
+		.vertical-play-infinite {
+			animation: verticalAnimation linear both running;
 			animation-iteration-count: 1;
 		}
 
-		@keyframes acrossAnimation {
+		@keyframes verticalAnimation {
 			100% {
-				transform: translateX(-100%);
+				transform: translateY(-100%);
 			}
 		}
-	}
 
-	.vertical {
-		flex-direction: column;
-		height: 36rpx;
-		line-height: 36rpx;
-	}
-
-	.vertical-play-infinite {
-		animation: verticalAnimation linear both running;
-		animation-iteration-count: 1;
-	}
-
-	@keyframes verticalAnimation {
-		100% {
-			transform: translateY(-100%);
+		.right-icon {
+			margin-left: 16rpx;
 		}
 	}
-
-	.right-icon {
-		margin-left: 16rpx;
-	}
-}
 </style>
