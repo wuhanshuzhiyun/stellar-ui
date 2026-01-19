0ee13692ae7451f77d630354de9ea71a2db08193
徐亚军 <850674935@qq.com>
2026-01-19 09:06:11 +0800

    feat(save): 修复文档

--- mp/app-update-demo/app-update-demo.vue
+++ mp/app-update-demo/app-update-demo.vue
@@ -22,18 +22,20 @@ export default {
 <template>
 	<view class="page">
 		<page-nav :autoBack="true" backColor="#000" titleAlignment="2" title="AppUpdate App更新"></page-nav>
-		<view class="description">
-			<view class="cmp-name">AppUpdate App更新</view>
-			<view class="cmp-desc">此组件用于APP更新功能</view>
-		</view>
-		<view class="type-block">
-			<view>01 组件类型</view>
-		</view>
-		<view class="demo-item">
-			<view class="title">基础使用</view>
-			<view class="item-block">
-				<ste-app-update ref="appUpdate" clientId="workbench_android" clientSecret="gkS6lEEncqAocYK2qsrvPQZykm3ISeMx"></ste-app-update>
-				<ste-button @click="checkForUpdates">检查更新</ste-button>
+		<view class="content">
+			<view class="description">
+				<view class="cmp-name">AppUpdate App更新</view>
+				<view class="cmp-desc">此组件用于APP更新功能</view>
+			</view>
+			<view class="type-block">
+				<view>01 组件类型</view>
+			</view>
+			<view class="demo-item">
+				<view class="title">基础使用</view>
+				<view class="item-block">
+					<ste-app-update ref="appUpdate" clientId="workbench_android" clientSecret="gkS6lEEncqAocYK2qsrvPQZykm3ISeMx"></ste-app-update>
+					<ste-button @click="checkForUpdates">检查更新</ste-button>
+				</view>
 			</view>
 		</view>
 	</view>
--- uni_modules/stellar-ui/components/ste-app-update/README.md
+++ uni_modules/stellar-ui/components/ste-app-update/README.md
@@ -13,19 +13,25 @@
 - 回调事`complete`取消，成功更新都会执行
 
 ```html
-<script setup lang="ts">
-    import type { RefAppUpdate } from '@/uni_modules/stellar-ui-plus/types/refComponents';
-    import { onHide } from '@dcloudio/uni-app';
-    import { ref } from 'vue';
-
-    const appUpdate = ref<RefAppUpdate>();
-    // 监听检查更新结果
-    const checkForUpdates = () => {
-        appUpdate.value?.start(({ code, name }, version) => {
-            console.log(`当前版本号：${version}`);
-            console.log(`服务器版本号：${code}；服务器版本名称${name}`);
-        });
-    };
+<script>
+export default {
+	name: 'AppUpdateDemo',
+	data() {
+		return {};
+	},
+	methods: {
+		// 监听检查更新结果
+		checkForUpdates() {
+			if (this.$refs.appUpdate) {
+				this.$refs.appUpdate.start((resVersion, version) => {
+					const { code, name } = resVersion;
+					console.log(`当前版本号：${version}`);
+					console.log(`服务器版本号：${code}；服务器版本名称${name}`);
+				});
+			}
+		},
+	},
+};
 </script>
 <template>
     <ste-app-update ref="appUpdate" clientId="workbench_android" clientSecret="gkS6lEEncqAocYK2qsrvPQZykm3ISeMx"></ste-app-update>
